8c1ecb0de3b4ddac2ccc5408b3a594746e7b5c58
Planeshifter <pburckhardt@outlook.com>
2017-03-28 02:38:46 -0400

    Update header bar

--- src/components/header_bar.js
+++ src/components/header_bar.js
@@ -1,10 +1,10 @@
 // MODULES //
 
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 import Radium from 'radium';
-import { 
-	Button, FormGroup, FormControl, Glyphicon, InputGroup, 
+import {
+	Button, FormGroup, FormControl, Glyphicon, InputGroup,
 	OverlayTrigger, Popover, ListGroupItem, ListGroup
 } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
@@ -16,9 +16,29 @@ import icon from './../../public/profile_pic.jpg';
 const RadiumLink = Radium( Link );
 
 
+// FUNCTIONS //
+
+function goToCreateCoursePage() {
+	browserHistory.replace( '/create-namespace' );
+}
+
+function goToCoursePage() {
+	browserHistory.replace( '/edit-namespace' );
+}
+
+
 // MAIN //
 
 class HeaderBar extends Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.logout = () => {
+
+		};
+	}
+
 	render() {
 
 		const containerStyle = {
@@ -38,7 +58,7 @@ class HeaderBar extends Component {
 			color: 'silver',
 			':hover': {
 				color: 'white'
-			}			
+			}
 		};
 
 		const namespacePopover = (
@@ -48,12 +68,11 @@ class HeaderBar extends Component {
 					<ListGroupItem>Chouldechova</ListGroupItem>
 					<ListGroupItem>Nagin</ListGroupItem>
 				</ListGroup>
-				<Button>Create Course</Button>
 			</Popover>
 		);
 
-		return ( 
-			<header style={{ 
+		return (
+			<header style={{
 				width: '100%',
 				height: '52px',
 				position: 'fixed',
@@ -62,65 +81,76 @@ class HeaderBar extends Component {
 				background: '#2a3e54',
 				zIndex: 100
 			}}>
-				<h1 style={{ 
+				<h1 style={{
 					float: 'left',
 					fontSize: '22px',
 					marginTop: '15px',
-					position: 'relative', 
-					marginLeft: '10px', 
+					position: 'relative',
+					marginLeft: '10px',
 				}} onClick={this.props.onDashboardClick}>
 					<RadiumLink to="/lessons" style={{
 						color: 'silver',
 						textDecoration: 'none',
 						':hover': {
 							color: 'white'
-						}	
+						}
 					}}> ISLE Dashboard </RadiumLink>
 				</h1>
-				<div style={{ 
+				<div style={{
 					marginLeft: '30px',
 					marginTop: '8px',
-					float: 'left', 
-					position: 'relative',			
+					float: 'left',
+					position: 'relative',
 				}}>
 					<OverlayTrigger trigger="click" placement="bottom" overlay={namespacePopover}>
-						<Button style={{ 
+						<Button style={{
 							float: 'left',
-							marginRight: '6px'
+							marginRight: '6px',
+							marginLeft: '6px'
 						}}>
 							<Glyphicon glyph="align-justify" />
-							<span> 90-711 <small>Statistical Methods</small></span>
+							<span><small>{this.props.currentNamespace.title}</small></span>
 						</Button>
 					</OverlayTrigger>
-					<Button style={{ float: 'left', marginRight: '6px' }}>
+					<Button
+						style={{ float: 'left', marginRight: '6px' }}
+						onClick={goToCoursePage}
+						disabled={!this.props.currentNamespace.title}
+					>
 						<Glyphicon glyph="edit" />
+					</Button>
+					<Button
+						style={{ float: 'left', marginRight: '6px' }}
+						onClick={goToCreateCoursePage}
+					>
+						<Glyphicon glyph="pencil" />
 					</Button>
 					<FormGroup style={{ width: '500px' }}>
 						<InputGroup>
 							<InputGroup.Addon>
 								<Glyphicon glyph="search" />
 							</InputGroup.Addon>
-							<FormControl style={{ 
+							<FormControl style={{
 								background: 'silver',
 								color: '#2a3e54'
 							}} type="text" placeholder="Search" />
 						</InputGroup>
 					</FormGroup>
 				</div>
-				<div style={{ 
-					float: 'right', 
+				<div style={{
+					float: 'right',
 					position: 'relative',
 					fontSize: '15px',
 					marginRight: '10px'
 				}}>
 					<div style={containerStyle}>
 						<div key="help" style={spanStyle} >
-							<a href="http://isledocs.philipp-burckhardt.com/" target="_blank" style={{
+							<a href="http://isledocs.com/" target="_blank" style={{
 								color: 'silver',
 								textDecoration: 'none',
 								':hover': {
 									color: 'white'
-								}	
+								}
 							}}>Help</a>
 						</div>
 					</div>
@@ -134,24 +164,29 @@ class HeaderBar extends Component {
 							marginTop: '10px'
 						}}></Image>
 						<div key="account" style={spanStyle} >
-							<RadiumLink to="/profile" style={{ 
-								color: 'silver',
-								textDecoration: 'none',
-								':hover': {
-									color: 'white'
-								}	
-							}}>{this.props.username}</RadiumLink>
-						</div>
-					</div>
-					<div style={containerStyle}>
-						<div key="logout" style={spanStyle} >
-							<RadiumLink key="logout" to="/login" style={{
+							<RadiumLink to="/profile" style={{
 								color: 'silver',
 								textDecoration: 'none',
 								':hover': {
 									color: 'white'
 								}
-							}}> Log out </RadiumLink>
+							}}>{this.props.username}</RadiumLink>
+						</div>
+					</div>
+					<div style={containerStyle}>
+						<div key="logout" style={spanStyle} >
+							<a
+								key="logoutButton"
+								href="#"
+								style={{
+									color: 'silver',
+									textDecoration: 'none',
+									':hover': {
+										color: 'white'
+									}
+								}}
+								onClick={this.props.onLogout}
+							> Log out </a>
 						</div>
 					</div>
 				</div>
