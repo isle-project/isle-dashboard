417af5d397e98e5e3eced95b183078422ae87dce
Planeshifter <pburckhardt@outlook.com>
2017-03-28 02:36:00 -0400

    Add routes

---
+++ src/routes.js
@@ -0,0 +1,48 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import { IndexRoute, Route } from 'react-router';
+import App from './containers/app.js';
+import VisibleCreateNamespace from './containers/visible_create_namespace.js';
+import VisibleEditNamespace from './containers/visible_edit_namespace.js';
+import ForgotPassword from './components/forgot_password.js';
+import VisibleLessonsPage from './containers/visible_lessons_page.js';
+import Lesson from './components/lesson.js';
+import VisibleLogin from './containers/visible_login.js';
+import Signup from './components/signup.js';
+import VisibleProfilePage from './containers/visible_profile_page.js';
+import AuthenticationBarrier from './containers/authentication_barrier.js';
+
+
+// EXPORTS //
+
+export default (
+	<Route path="/" component={App}>
+		<IndexRoute component={VisibleLogin} />
+		<Route component={AuthenticationBarrier}>
+			<Route
+				path="create-namespace"
+				component={VisibleCreateNamespace}
+			/>
+			<Route
+				path="edit-namespace"
+				component={VisibleEditNamespace}
+			/>
+			<Route
+				path="profile"
+				component={VisibleProfilePage}
+			/>
+			<Route
+				path="lessons"
+				component={VisibleLessonsPage}
+			/>
+			<Route
+				path="lessons/:lessonId"
+				component={Lesson}
+			/>
+		</Route>
+		<Route path="login" component={VisibleLogin}/>
+		<Route path="signup" component={Signup}/>
+		<Route path="forgot-password" component={ForgotPassword}/>
+	</Route>
+);
