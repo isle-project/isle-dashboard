ded23a5c43c41af2234c23e89be25a48fa2fe4f8
Planeshifter <pburckhardt@outlook.com>
2017-03-28 02:39:21 -0400

    Add modal message component

---
+++ src/components/message_modal.js
@@ -0,0 +1,28 @@
+// MODULES //
+
+import React, { Component } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+
+// MAIN //
+
+const MsgModal = ( props ) => (
+	<Modal show={props.show} onHide={props.close}>
+		<Modal.Header closeButton>
+			<Modal.Title>{props.title}</Modal.Title>
+		</Modal.Header>
+		<Modal.Body>
+			{props.message}
+		</Modal.Body>
+		<Modal.Footer>
+			<Button onClick={props.close}>
+				Close
+			</Button>
+		</Modal.Footer>
+	</Modal>
+);
+
+
+// EXPORTS //
+
+export default MsgModal;
