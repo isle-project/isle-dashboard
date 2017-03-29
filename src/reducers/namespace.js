039c46f0838b8db64da938c52c7b374ce0ae6660
Planeshifter <pburckhardt@outlook.com>
2017-03-29 01:27:48 -0400

    Also handle namespace id

--- src/reducers/namespace.js
+++ src/reducers/namespace.js
@@ -6,6 +6,7 @@ import * as types from './../constants/action_types.js';
 // VARIABLES //
 
 const initialState = {
+	id: null,
 	title: '',
 	description: '',
 	owners: ''
@@ -18,10 +19,13 @@ export default function namespace( state = initialState, action ) {
 	switch ( action.type ) {
 	case types.CHANGED_NAMESPACE:
 		return Object.assign({}, state, {
+			_id: action.payload._id,
 			title: action.payload.title,
 			description: action.payload.description,
 			owners: action.payload.owners
 		});
+	case types.DELETED_CURRENT_NAMESPACE:
+		return initialState;
 	default:
 		return state;
 	}
