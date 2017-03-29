73e48eaaf19d4cd58c21aea33cddc544d68f70e4
Planeshifter <pburckhardt@outlook.com>
2017-03-29 01:46:32 -0400

    Add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,13 +23,60 @@ export function loggedOut() {
 	};
 }
 
-export function changedNamespace({ title, owners, description }) {
+export function changedNamespace({ title, owners, description, _id }) {
 	return {
 		type: types.CHANGED_NAMESPACE,
 		payload: {
 			title,
 			description,
-			owners
+			owners,
+			_id
+		}
+	};
+}
+
+export function retrievedNamespaces( namespaces ) {
+	return {
+		type: types.RETRIEVED_NAMESPACES,
+		payload: {
+			namespaces
+		}
+	};
+}
+
+export function deletedCurrentNamespace() {
+	return {
+		type: types.DELETED_CURRENT_NAMESPACE
+	};
+}
+
+export function updateCurrentNamespace({ title, owners, description, _id }) {
+	return {
+		type: types.UPDATE_CURRENT_NAMESPACE,
+		payload: {
+			title,
+			description,
+			owners,
+			_id
+		}
+	};
+}
+
+export function addNotification({ message, level }) {
+	return {
+		type: types.ADD_NOTIFICATION,
+		payload: {
+			message,
+			level
+		}
+	};
+}
+
+export function updateUser({ name }) {
+	return {
+		type: types.USER_UPDATED,
+		payload: {
+			name
 		}
 	};
 }
