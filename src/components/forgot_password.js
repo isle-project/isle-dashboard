8257bfd7f2addd2c43edf22c71e811af701055d4
Planeshifter <pburckhardt@outlook.com>
2017-03-29 02:39:15 -0400

    Add functionality

--- src/components/forgot_password.js
+++ src/components/forgot_password.js
@@ -1,14 +1,37 @@
 // MODULES //
 
 import React, { Component } from 'react';
-import { Button, Col, ControlLabel, FormControl, FormGroup, Form, Panel } from 'react-bootstrap';
+import { Button, ControlLabel, FormControl, FormGroup, Form, Panel } from 'react-bootstrap';
 import { Link } from 'react-router';
+import request from 'request';
 import './login.css';
 
 
 // MAIN //
 
 class ForgotPassword extends Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			email: ''
+		};
+
+		this.handleClick = () => {
+			request.get( 'http://localhost:3000/forgot_password', {
+				qs: {
+					email: this.state.email
+				}
+			}, function( error, response, body ) {
+				if ( error ) {
+					return error;
+				}
+				console.log( body );
+			});
+		};
+	}
+
 	render() {
 		return (
 			<div className="login">
@@ -18,9 +41,20 @@ class ForgotPassword extends Component {
 						<FormGroup controlId="formHorizontalEmail">
 							<ControlLabel>Email Address</ControlLabel>
 							{'   '}
-							<FormControl type="email" placeholder="Enter Email" />
+							<FormControl
+								type="email"
+								placeholder="Enter Email"
+								value={this.state.email}
+								onChange={( event )=>{
+									const target = event.target;
+									const value = target.value;
+									this.setState({
+										email:  value
+									});
+								}}
+							/>
 							{'   '}
-							<Button bsStyle="primary" type="submit">Reset</Button>
+							<Button onClick={this.handleClick} bsStyle="primary" type="submit">Reset</Button>
 						</FormGroup>
 					</Form>
 					<div style={{ marginTop: 20 }}>
