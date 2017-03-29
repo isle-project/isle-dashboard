5a2c570a8746d125ff906922b113a95a9894abb0
Planeshifter <pburckhardt@outlook.com>
2017-03-29 02:40:48 -0400

    Add dispatch methods

--- src/containers/visible_edit_namespace.js
+++ src/containers/visible_edit_namespace.js
@@ -1,9 +1,11 @@
 // MODULES //
 
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import request from 'request';
+import { browserHistory } from 'react-router';
 import EditNamespace from './../components/edit_namespace.js';
-import { updatedNamespace, deletedNamespace } from './../actions';
+import * as actions from './../actions';
 
 
 // EXPORTS //
@@ -19,7 +21,54 @@ function mapStateToProps( state ) {
 
 function  mapDispatchToProps( dispatch ) {
 	return {
-
+		getNamespaces: ( token ) => {
+			request.get( 'http://localhost:3000/get_namespaces', {
+				headers: {
+					'Authorization': 'JWT ' + token
+				}
+			}, function( error, response, body ) {
+				if ( error ) {
+					return error;
+				}
+				body = JSON.parse( body );
+				dispatch( actions.retrievedNamespaces( body.namespaces ) );
+			});
+		},
+		deleteCurrentNamespace: ( id, token ) => {
+			request.get( 'http://localhost:3000/delete_namespace', {
+				qs: {
+					id
+				},
+				headers: {
+					'Authorization': 'JWT ' + token
+				}
+			}, ( err ) => {
+				if ( !err ) {
+					browserHistory.replace( '/lessons' );
+					dispatch( actions.deletedCurrentNamespace() );
+					dispatch( actions.addNotification({
+						message: 'Course successfully deleted',
+						level: 'success'
+					}) );
+				}
+			});
+		},
+		updateCurrentNamespace: ( ns, clbk ) => {
+			request.post( 'http://localhost:3000/update_namespace', {
+				form: {
+					ns
+				},
+				headers: {
+					'Authorization': 'JWT ' + ns.token
+				}
+			}, ( err ) => {
+				if ( err ) {
+					return err;
+				}
+				clbk();
+				dispatch( actions.changedNamespace( ns ) );
+			});
+		}
 	};
 } // end FUNCTION mapStateToProps()
 
