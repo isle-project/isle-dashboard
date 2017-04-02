bea361440a44d3a2620abe8480d9960ed7e62984
Planeshifter <pburckhardt@outlook.com>
2017-04-02 02:02:42 -0400

    Retrieve lessons from server

--- src/components/lessons_page.js
+++ src/components/lessons_page.js
@@ -3,9 +3,10 @@
 import React, { Component } from 'react';
 import {
 	Button, ButtonGroup, ButtonToolbar, Glyphicon,
-	Grid, Row, Col, ControlLabel, Label, Modal, Panel
+	Grid, Row, Col, ControlLabel, Jumbotron, Label, Modal, Panel
 } from 'react-bootstrap';
 import chunkify from 'compute-chunkify';
+import isArray from '@stdlib/utils/is-array';
 import icon from './../../public/screenshot.jpg';
 import './image.css';
 
@@ -19,11 +20,11 @@ const DeleteModel = ( props ) =>
 			<Modal.Title>Delete?</Modal.Title>
 		</Modal.Header>
 		<Modal.Body>
-			Are you sure that you want to delete the lesson with the name "{props.name}"?
+			Are you sure that you want to delete the lesson with the name "{props.title}"?
 		</Modal.Body>
 		<Modal.Footer>
 			<Button onClick={props.close}>Cancel</Button>
-			<Button bsStyle="danger">Delete</Button>
+			<Button bsStyle="danger" onClick={props.delete} >Delete</Button>
 		</Modal.Footer>
 	</Modal>;
 
@@ -47,10 +48,20 @@ class Lesson extends Component {
 			this.setState({ showDeleteModal: false });
 		};
 
+		this.delete = () => {
+			console.log( this.props );
+			this.props.deleteLesson({
+				lessonName: this.props.title,
+				namespaceName: this.props.namespace,
+				token: this.props.token
+			});
+			this.closeDeleteModal();
+		};
+
 	}
 
 	render() {
-		const labelStyle = this.props.status === 'active' ? 'success' : 'warning';
+		const labelStyle = this.props.active === true ? 'success' : 'warning';
 		return (
 			<div>
 				<Panel>
@@ -65,7 +76,7 @@ class Lesson extends Component {
 							}}
 						/>
 						<div className="overlay">
-							<h2>{this.props.name}</h2>
+							<h2>{this.props.title}</h2>
 							<a className="info" href={this.props.url} target="_blank" >Open Lesson</a>
 						</div>
 					</div>
@@ -80,11 +91,11 @@ class Lesson extends Component {
 								<Label bsStyle={labelStyle} style={{
 									fontSize: '18px',
 									float: 'right'
-								}}>{this.props.status}</Label>
+								}}>{this.props.active ? 'Active' : 'Disabled'}</Label>
 							</ControlLabel>
 						</ButtonGroup>
 					</ButtonToolbar>
-					<DeleteModel {...this.props} show={this.state.showDeleteModal} close={this.closeDeleteModal}/>
+					<DeleteModel {...this.props} show={this.state.showDeleteModal} close={this.closeDeleteModal} delete={this.delete} />
 				</Panel>
 			</div>
 		);
@@ -96,106 +107,64 @@ class Lesson extends Component {
 
 class LessonsPage extends Component {
 
-	constructor() {
+	constructor( props ) {
+		super( props );
+		props.getLessons( props.namespace.title );
+	}
 
-		super();
-
-		this.state = {
-			lessons: [
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Binomial',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/binomial/'
-				},
-				{
-					'name': 'Central Limit Theorem',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/clt/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Binomial',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/binomial/'
-				},
-				{
-					'name': 'Central Limit Theorem',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/clt/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				},
-				{
-					'name': 'Normal Distribution',
-					'status': 'active',
-					'url': 'http://isle.heinz.cmu.edu/90-711/normal/'
-				}
-			]
-		};
-
+	componentWillReceiveProps( nextProps ) {
+		if (
+			nextProps.namespace.title !== this.props.namespace.title
+		) {
+			this.props.getLessons( nextProps.namespace.title );
+		}
 	}
 
 	render() {
-
-		let chunks = chunkify( this.state.lessons, 4 );
-		for ( let i = 0; i < chunks.length; i++ ) {
-			let chunk = chunks[ i ];
-			for ( let j = 0; j < chunk.length; j++ ) {
-				if ( chunk[ j ]) {
-					chunk[ j ] = <Col key={`cell${i}${j}`} xs={6} md={3}>
-						<Lesson {...chunk[ j ]} />
-					</Col>;
-				} else {
-					chunk[ j ] = null;
+		const { lessons } = this.props.namespace;
+		if ( isArray( lessons ) ) {
+			if ( lessons.length === 0 ) {
+				return <Jumbotron style={{ position: 'relative', top: 70, textAlign: 'left', paddingLeft: 20 }}>
+					<h1>No Lessons Found</h1>
+					<p>The selected course does not contain any lessons. You can upload lessons from the ISLE editor.</p>
+				</Jumbotron>;
+			}
+			let chunks = chunkify( lessons, 4 );
+			for ( let i = 0; i < chunks.length; i++ ) {
+				let chunk = chunks[ i ];
+				for ( let j = 0; j < chunk.length; j++ ) {
+					if ( chunk[ j ]) {
+						chunk[ j ] = <Col key={`cell${i}${j}`} xs={6} md={3}>
+							<Lesson
+								{...chunk[ j ]}
+								deleteLesson={this.props.deleteLesson}
+								token={this.props.user.token}
+							/>
+						</Col>;
+					} else {
+						chunk[ j ] = null;
+					}
 				}
 			}
+			return (
+				<Grid style={{ position: 'relative', top: 70 }} >
+					{chunks.map( ( chunk, id ) => {
+						return (
+							<Row key={`row${id}`} >
+								{chunk}
+							</Row>
+						);
+					})}
+				</Grid>
+			);
 		}
-
 		return (
-			<Grid style={{ position: 'relative', top: 70 }} >
-				{chunks.map( ( chunk, id ) => {
-					return (
-						<Row key={`row${id}`} >
-							{chunk}
-						</Row>
-					);
-				})}
-			</Grid>
+			<Jumbotron style={{ position: 'relative', top: 70, textAlign: 'left', paddingLeft: 20 }}>
+				<h1>No Course Selected</h1>
+				<p>Open an existing course by selecting one from the dropdown menu above at <Glyphicon glyph="align-justify" /> or create a new one under <Glyphicon glyph="pencil" />.</p>
+			</Jumbotron>
 		);
 	}
-
 }
 
 
--- src/containers/visible_lessons_page.js
+++ src/containers/visible_lessons_page.js
@@ -2,7 +2,9 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import request from 'request';
 import LessonsPage from './../components/lessons_page.js';
+import * as actions from './../actions';
 
 
 // EXPORTS //
@@ -11,12 +13,66 @@ const VisibleLessonsPage = connect( mapStateToProps, mapDispatchToProps )( Lesso
 
 function mapStateToProps( state ) {
 	return {
-		user: state.user
+		user: state.user,
+		namespace: state.namespace
 	};
 }
 
-function  mapDispatchToProps() {
-	return {};
+function  mapDispatchToProps( dispatch ) {
+	return {
+		deleteLesson: ({ lessonName, namespaceName, token }) => {
+			if ( namespaceName && lessonName ) {
+				request.get( 'http://localhost:3000/delete_lesson', {
+					qs: {
+						namespaceName,
+						lessonName
+					},
+					headers: {
+						'Authorization': 'JWT ' + token
+					}
+				}, function ( err, res ) {
+					if ( err ) {
+						return dispatch( actions.addNotification({
+							message: err.message,
+							level: 'error'
+						}) );
+					}
+					let msg = JSON.parse( res.body ).message;
+					if ( res.statusCode >= 400 ) {
+						return dispatch( actions.addNotification({
+							message: msg,
+							level: 'error'
+						}) );
+					}
+					dispatch( actions.deletedLesson( lessonName ) );
+					dispatch( actions.addNotification({
+						message: msg,
+						level: 'success'
+					}) );
+				});
+			}
+		},
+		getLessons: ( namespaceName ) => {
+			if ( namespaceName ) {
+				request.get( 'http://localhost:3000/get_lessons', {
+					qs: {
+						namespaceName
+					}
+				}, function( error, response, body ) {
+					if ( error ) {
+						return error;
+					}
+					body = JSON.parse( body );
+					let lessons = body.lessons;
+					lessons = lessons.map( lesson => {
+						lesson.url = 'http://localhost:3000/' + namespaceName + '/' + lesson.title;
+						return lesson;
+					});
+					dispatch( actions.retrievedLessons( lessons ) );
+				});
+			}
+		}
+	};
 }
 
 export default VisibleLessonsPage;
