6aeaaad4ef7b6f23bba7b80e701d373fd8c9dfed
Planeshifter <pburckhardt@outlook.com>
2017-04-03 15:12:51 -0400

    Change port

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,4 +1,4 @@
-export const LOCAL = 'http://localhost:3000';
+export const LOCAL = 'http://localhost:17777';
 export const REMOTE = 'http://isle.heinz.cmu.edu/server';
 
 export default ( process.env.NODE_ENV === 'development' ? LOCAL : REMOTE );
