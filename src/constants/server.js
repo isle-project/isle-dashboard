f7075c6852a7b2917bb8d0eaa9c79b741d001e22
Planeshifter <pburckhardt@outlook.com>
2017-09-08 17:42:44 -0400

    Switch to HTTPS

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,4 +1,4 @@
 export const LOCAL = 'http://localhost:17777';
-export const REMOTE = 'http://isle.heinz.cmu.edu';
+export const REMOTE = 'https://isle.heinz.cmu.edu';
 
 export default ( process.env.NODE_ENV === 'development' ? LOCAL : REMOTE );
