efdaa669a1952c707afeb2396db5964b6ad1b4c2
Planeshifter <pburckhardt@outlook.com>
2018-10-02 14:33:18 -0400

    Update dependencies and scripts

--- scripts/build.js
+++ scripts/build.js
@@ -13,11 +13,19 @@ process.on('unhandledRejection', err => {
 
 // Ensure environment variables are read.
 require('../config/env');
+// @remove-on-eject-begin
+// Do the preflight check (only happens before eject).
+const verifyPackageTree = require('./utils/verifyPackageTree');
+if (process.env.SKIP_PREFLIGHT_CHECK !== 'true') {
+	verifyPackageTree();
+}
+// @remove-on-eject-end
 
 const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
+const bfj = require('bfj');
 const config = require('../config/webpack.config.prod');
 const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
@@ -29,19 +37,32 @@ const printBuildError = require('react-dev-utils/printBuildError');
 const measureFileSizesBeforeBuild =
 	FileSizeReporter.measureFileSizesBeforeBuild;
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
+const useYarn = fs.existsSync(paths.yarnLockFile);
 
 // These sizes are pretty large. We'll warn for bundles exceeding them.
 const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
+
+const isInteractive = process.stdout.isTTY;
 
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 	process.exit(1);
 }
 
-// First, read the current file sizes in build directory.
-// This lets us display how much they changed later.
-measureFileSizesBeforeBuild(paths.appBuild)
+// Process CLI arguments
+const argv = process.argv.slice(2);
+const writeStatsJson = argv.indexOf('--stats') !== -1;
+
+// We require that you explictly set browsers and do not fall back to
+// browserslist defaults.
+const { checkBrowsers } = require('react-dev-utils/browsersHelper');
+checkBrowsers(paths.appPath, isInteractive)
+	.then(() => {
+		// First, read the current file sizes in build directory.
+		// This lets us display how much they changed later.
+		return measureFileSizesBeforeBuild(paths.appBuild);
+	})
 	.then(previousFileSizes => {
 		// Remove all content but keep the directory so that
 		// if you're in it, you don't end up in Trash
@@ -88,7 +109,8 @@ measureFileSizesBeforeBuild(paths.appBuild)
 				appPackage,
 				publicUrl,
 				publicPath,
-				buildFolder
+				buildFolder,
+				useYarn
 			);
 		},
 		err => {
@@ -96,7 +118,13 @@ measureFileSizesBeforeBuild(paths.appBuild)
 			printBuildError(err);
 			process.exit(1);
 		}
-	);
+	)
+	.catch(err => {
+		if (err && err.message) {
+			console.log(err.message);
+		}
+		process.exit(1);
+	});
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
@@ -105,10 +133,20 @@ function build(previousFileSizes) {
 	let compiler = webpack(config);
 	return new Promise((resolve, reject) => {
 		compiler.run((err, stats) => {
+			let messages;
 			if (err) {
-				return reject(err);
+				if (!err.message) {
+					return reject(err);
+				}
+				messages = formatWebpackMessages({
+					errors: [err.message],
+					warnings: [],
+				});
+			} else {
+				messages = formatWebpackMessages(
+					stats.toJson({ all: false, warnings: true, errors: true })
+				);
 			}
-			const messages = formatWebpackMessages(stats.toJson({}, true));
 			if (messages.errors.length) {
 				// Only keep the first error. Others are often indicative
 				// of the same problem, but confuse the reader with noise.
@@ -131,11 +169,20 @@ function build(previousFileSizes) {
 				);
 				return reject(new Error(messages.warnings.join('\n\n')));
 			}
-			return resolve({
+
+			const resolveArgs = {
 				stats,
 				previousFileSizes,
 				warnings: messages.warnings,
-			});
+			};
+			if (writeStatsJson) {
+				return bfj
+					.write(paths.appBuild + '/bundle-stats.json', stats.toJson())
+					.then(() => resolve(resolveArgs))
+					.catch(error => reject(new Error(error)));
+			}
+
+			return resolve(resolveArgs);
 		});
 	});
 }
