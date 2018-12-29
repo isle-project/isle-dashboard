733eb9cfc38f85ac62168cb93d67f7aa82a0b6ea
Planeshifter <pburckhardt@outlook.com>
2018-12-29 13:20:19 +0100

    Add src to resolve paths

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -187,7 +187,11 @@ module.exports = {
 		// We placed these paths second because we want `node_modules` to "win"
 		// if there are any conflicts. This matches Node resolution mechanism.
 		// https://github.com/facebook/create-react-app/issues/253
-		modules: ['node_modules',resolve( './node_modules/@stdlib/stdlib/lib/node_modules' )].concat(
+		modules: [
+			'node_modules',
+			resolve( './node_modules/@stdlib/stdlib/lib/node_modules' ),
+			paths.appSrc
+		].concat(
 			// It is guaranteed to exist because we tweak it in `env.js`
 			process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
 		),
