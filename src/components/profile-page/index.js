59eadb181e60dca630b77c54e295cddb7dbc8a5e
Planeshifter <pburckhardt@outlook.com>
2018-12-31 12:25:50 +0100

    Add Plotly and render lesson times

--- src/components/profile-page/index.js
+++ src/components/profile-page/index.js
@@ -3,8 +3,10 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card, Media, Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import Plotly from 'react-plotly.js';
 import keys from '@stdlib/utils/keys';
 import isArray from '@stdlib/assert/is-array';
+import round from '@stdlib/math/base/special/round';
 import iterMean from '@stdlib/stats/iterators/mean';
 import array2iterator from '@stdlib/array/to-iterator';
 import formatTime from 'utils/format_time.js';
@@ -123,16 +125,67 @@ class ProfilePage extends Component {
 			return null;
 		}
 		const data = this.props.user.lessonData;
-		const durations = [];
+		let durations = [];
+		const names = [];
+		const texts = [];
 		for ( let i = 0; i < lessons.length; i++ ) {
 			const lesson = lessons[ i ];
 			if ( data[ lesson._id ] ) {
 				durations.push( data[ lesson._id ].spentTime );
+				names.push( lesson.title );
+				texts.push( `Completed: ${round( data[ lesson._id ].progress*100.0 )}%` );
 			}
 		}
 		const it = array2iterator( durations );
 		const avg = iterMean( it );
-		return <div><label>Average time spent:</label>{formatTime( avg )}</div>;
+		durations = durations.map( d => {
+			return d / ( 1000*60 );
+		});
+		return (
+			<div style={{ padding: '5px' }}>
+				<label>Average time spent: </label><span>{` ${formatTime( avg )}`}</span>
+				<Plotly
+					data={[{
+						x: durations,
+						y: names,
+						text: texts,
+						type: 'bar',
+						orientation: 'h',
+						marker: {
+							color: '#0069d9',
+							opacity: 0.6,
+							line: {
+								color: 'rgb(8,48,107)',
+								width: 1.5
+							}
+						}
+					}]}
+					config={{
+						displayModeBar: false,
+						displaylogo: false
+					}}
+					layout={{
+						plot_bgcolor: 'rgba(0,0,0,0.0)',
+						paper_bgcolor: 'rgba(0,0,0,0.0)',
+						xaxis: {
+							title: 'Time (in min)',
+							fixedrange: true
+						},
+						yaxis: {
+							title: 'Lesson Name',
+							fixedrange: true
+						},
+						title: 'Time spent per lesson',
+						autosize: true
+					}}
+					useResizeHandler={true}
+					style={{
+						width: '100%',
+						height: '100%'
+					}}
+				/>
+			</div>
+		);
 	}
 
 	renderFiles() {
