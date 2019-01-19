feae0899886b80edfe2953521357ffd95a908852
Planeshifter <pburckhardt@outlook.com>
2019-01-19 14:42:17 -0500

    Convert to tabs

--- src/components/profile-page/profile_pic_modal.js
+++ src/components/profile-page/profile_pic_modal.js
@@ -7,15 +7,17 @@ import PropTypes from 'prop-types';
 import path from 'path';
 
 
+// FUNCTIONS //
+
 function getResizedCanvas(canvas, newWidth, newHeight) {
-    const tmpCanvas = document.createElement('canvas');
-    tmpCanvas.width = newWidth;
-    tmpCanvas.height = newHeight;
+	const tmpCanvas = document.createElement('canvas');
+	tmpCanvas.width = newWidth;
+	tmpCanvas.height = newHeight;
 
-    const ctx = tmpCanvas.getContext('2d');
-    ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, newWidth, newHeight);
+	const ctx = tmpCanvas.getContext('2d');
+	ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, newWidth, newHeight);
 
-    return tmpCanvas;
+	return tmpCanvas;
 }
 
 // MAIN //
