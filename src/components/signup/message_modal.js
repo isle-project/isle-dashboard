e65a859563f70c43e4d6fdb183601d81ce958a0d
Planeshifter <pburckhardt@outlook.com>
2019-01-25 17:32:51 -0500

    Update react-bootstrap

--- src/components/signup/message_modal.js
+++ src/components/signup/message_modal.js
@@ -1,8 +1,8 @@
 // MODULES //
 
 import React from 'react';
-import Button from 'react-bootstrap/lib/Button';
-import Modal from 'react-bootstrap/lib/Modal';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { withRouter } from 'react-router';
 
 
