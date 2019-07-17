5b219aa1d0421eecacd4f8efe47bb6c3a235aee0
Planeshifter <pburckhardt@outlook.com>
2019-07-17 10:05:28 -0400

    Sort lessons with numbers more sensibly

--- src/utils/sort_lessons.js
+++ src/utils/sort_lessons.js
@@ -1,3 +1,10 @@
+// VARIABLES //
+
+const OPTS = {
+	numeric: true // Use numeric collation such that "1" < "2" < "10"...
+};
+
+
 // MAIN //
 
 function sortLessons( lessons, search ) {
@@ -7,35 +14,35 @@ function sortLessons( lessons, search ) {
 			lessons.sort( ( a, b ) => {
 				const x = a.namespace + a.title;
 				const y = b.namespace + b.title;
-				return x.localeCompare( y );
+				return x.localeCompare( y, void 0, OPTS );
 			});
 		} else {
 			lessons.sort( ( a, b ) => {
 				const x = a.namespace + a.title;
 				const y = b.namespace + b.title;
-				return y.localeCompare( x );
+				return y.localeCompare( x, void 0, OPTS );
 			});
 		}
 	}
 	else if ( type === 'created_at' ) {
 		if ( direction === 'ascending' ) {
 			lessons.sort( ( a, b ) => {
-				return a.createdAt.localeCompare( b.createdAt );
+				return a.createdAt.localeCompare( b.createdAt, void 0, OPTS );
 			});
 		} else {
 			lessons.sort( ( a, b ) => {
-				return b.createdAt.localeCompare( a.createdAt );
+				return b.createdAt.localeCompare( a.createdAt, void 0, OPTS );
 			});
 		}
 	}
 	else if ( type === 'updated_at' ) {
 		if ( direction === 'ascending' ) {
 			lessons.sort( ( a, b ) => {
-				return a.updatedAt.localeCompare( b.updatedAt );
+				return a.updatedAt.localeCompare( b.updatedAt, void 0, OPTS );
 			});
 		} else {
 			lessons.sort( ( a, b ) => {
-				return b.updatedAt.localeCompare( a.updatedAt );
+				return b.updatedAt.localeCompare( a.updatedAt, void 0, OPTS );
 			});
 		}
 	}
