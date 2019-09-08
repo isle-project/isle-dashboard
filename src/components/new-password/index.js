a8880574cfd209900806fd69534476f8731e0bcc
Planeshifter <pburckhardt@outlook.com>
2019-09-08 15:32:31 -0400

    Fix styling

--- src/components/new-password/index.js
+++ src/components/new-password/index.js
@@ -1,7 +1,7 @@
 // MODULES //
 
 import React, { Component } from 'react';
-import { Button, Card, Col, FormLabel, FormControl, FormGroup, Form, Modal, OverlayTrigger, Overlay, Popover, Tooltip } from 'react-bootstrap';
+import { Button, Card, Col, Row, FormLabel, FormControl, FormGroup, Form, Modal, OverlayTrigger, Overlay, Popover, Tooltip } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
@@ -131,69 +131,79 @@ class NewPassword extends Component {
 			<div>
 				<div className="login">
 					<Card style={{ opacity: 0.9 }}>
-						<Card.Title>
-							<small>Choose a new Password</small>
-						</Card.Title>
-						<Form>
-							<FormGroup>
-								<Col sm={2}>
-									<FormLabel>Token</FormLabel>
-								</Col>
-								<Col sm={10}>
-									<FormControl
-										name="token"
-										type="token"
-										onChange={this.handleInputChange}
-										value={this.state.token}
-									/>
-								</Col>
-							</FormGroup>
-							<OverlayTrigger placement="right" overlay={createTooltip( 'Please enter a new password with at least six characters' )}>
+						<Card.Header>
+							<Card.Title as="h1">
+								<small>Choose a new Password</small>
+							</Card.Title>
+						</Card.Header>
+						<Card.Body>
+							<Form>
+								<FormGroup>
+									<Row>
+										<Col sm={2}>
+											<FormLabel>Token</FormLabel>
+										</Col>
+										<Col sm={10}>
+											<FormControl
+												name="token"
+												type="token"
+												onChange={this.handleInputChange}
+												value={this.state.token}
+											/>
+										</Col>
+									</Row>
+								</FormGroup>
+								<OverlayTrigger placement="right" overlay={createTooltip( 'Please enter a new password with at least six characters' )}>
+									<FormGroup
+										controlId="form-password"
+										validationState={this.getPasswordValidationState()}
+									>
+										<Row>
+											<Col sm={2}>
+												<FormLabel>Password</FormLabel>
+											</Col>
+											<Col sm={10}>
+												<FormControl
+													name="password"
+													type="password"
+													placeholder="Enter new password"
+													onChange={this.handleInputChange}
+													maxLength={30}
+													minLength={6}
+												/>
+												<FormControl.Feedback />
+											</Col>
+										</Row>
+									</FormGroup>
+								</OverlayTrigger>
 								<FormGroup
-									controlId="form-password"
+									controlId="form-password-confirmation"
 									validationState={this.getPasswordValidationState()}
 								>
-									<Col sm={2}>
-										<FormLabel>Password</FormLabel>
-									</Col>
-									<Col sm={10}>
-										<FormControl
-											name="password"
-											type="password"
-											placeholder="Enter new password"
-											onChange={this.handleInputChange}
-											maxLength={30}
-											minLength={6}
-										/>
-										<FormControl.Feedback />
-									</Col>
+									<Row>
+										<Col sm={{ span: 10, offset: 2 }}>
+											<FormControl
+												name="passwordRepeat"
+												type="password"
+												placeholder="Confirm new password"
+												onChange={this.handleInputChange}
+												maxLength={30}
+												minLength={6}
+											/>
+											<FormControl.Feedback />
+										</Col>
+									</Row>
 								</FormGroup>
-							</OverlayTrigger>
-							<FormGroup
-								controlId="form-password-confirmation"
-								validationState={this.getPasswordValidationState()}
-							>
-								<Col sm={{ span: 10, offset: 2 }}>
-									<FormControl
-										name="passwordRepeat"
-										type="password"
-										placeholder="Confirm new password"
-										onChange={this.handleInputChange}
-										maxLength={30}
-										minLength={6}
-									/>
-									<FormControl.Feedback />
-								</Col>
-							</FormGroup>
-							<FormGroup>
-								<Button
-									variant="primary"
-									type="submit"
-									onClick={this.handleSubmit}
-									className="centered"
-								>Confirm</Button>
-							</FormGroup>
-						</Form>
+								<FormGroup>
+									<Button
+										variant="primary"
+										type="submit"
+										onClick={this.handleSubmit}
+										className="centered"
+									>Confirm</Button>
+								</FormGroup>
+							</Form>
+						</Card.Body>
 					</Card>
 				</div>
 				<MsgModal
