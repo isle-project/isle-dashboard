c8c7c14540d14a1e49ab38e6421f40ace4f47d84
Planeshifter <pburckhardt@outlook.com>
2020-01-28 15:53:04 -0500

    Update profile pic uploading

--- src/components/profile-page/profile_pic_modal.js
+++ src/components/profile-page/profile_pic_modal.js
@@ -9,14 +9,12 @@ import path from 'path';
 
 // FUNCTIONS //
 
-function getResizedCanvas(canvas, newWidth, newHeight) {
-	const tmpCanvas = document.createElement('canvas');
+function getResizedCanvas( canvas, newWidth, newHeight ) {
+	const tmpCanvas = document.createElement( 'canvas' );
 	tmpCanvas.width = newWidth;
 	tmpCanvas.height = newHeight;
-
-	const ctx = tmpCanvas.getContext('2d');
-	ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, newWidth, newHeight);
-
+	const ctx = tmpCanvas.getContext( '2d' );
+	ctx.drawImage( canvas, 0, 0, canvas.width, canvas.height, 0, 0, newWidth, newHeight );
 	return tmpCanvas;
 }
 
@@ -27,17 +25,26 @@ class ProfilePicModal extends Component {
 	constructor( props ) {
 		super( props );
 
+		let ext;
+		if ( props.user.picture ) {
+			ext = props.user.picture.substr( props.user.picture.lastIndexOf( '.' ) );
+		} else {
+			ext = null;
+		}
 		this.state = {
 			actualFile: props.user.picture,
 			zoom: 1.2,
-			ext: null
+			ext,
+			rotate: 0
 		};
 	}
 
 	componentDidUpdate( prevProps ) {
 		if ( this.props.user.picture !== prevProps.user.picture ) {
 			this.setState({
-				actualFile: this.props.user.picture
+				actualFile: this.props.user.picture,
+				rotate: 0,
+				zoom: 1.2
 			});
 		}
 	}
@@ -45,13 +52,13 @@ class ProfilePicModal extends Component {
 	handleUpload = () => {
 		if ( this.editor ) {
 			const canvas = this.editor.getImageScaledToCanvas();
-
 			canvas.toBlob( img => {
 				const avatarData = new FormData();
 				const thumbnailData = new FormData();
-				const tn = getResizedCanvas(canvas, 80, 80);
+				const tn = getResizedCanvas( canvas, 80, 80 );
 				tn.toBlob( thumbnail => {
-					const filename = this.props.user.id + this.state.ext;
+					const id = this.props.user.id;
+					const filename = `${id}_${this.state.zoom}_${this.state.rotate}${this.state.ext}`;
 					const thumbnailName = filename;
 					avatarData.append( 'avatar', img, filename );
 					thumbnailData.append( 'thumbnail', thumbnail, thumbnailName );
@@ -98,15 +105,33 @@ class ProfilePicModal extends Component {
 					scale={this.state.zoom}
 					rotate={0}
 				/>
-				<Form.Group controlId="form-zoom">
-					<Form.Label>Zoom</Form.Label>
-					<FormControl
-						step={0.05} type="range" defaultValue={1} min={0.5} max={3}
-						onChange={this.changeZoom}
-					/>
+				<Form.Group as={Row} controlId="form-zoom">
+					<Form.Label column sm="2">Zoom</Form.Label>
+					<Col sm="10">
+						<FormControl
+							step={0.05}
+							type="range"
+							defaultValue={1} min={0.5} max={3}
+							onChange={this.changeZoom}
+						/>
+					</Col>
 				</Form.Group>
 			</Fragment>
 		);
+	}
+
+	rotateLeft = () => {
+		const oldRotate = this.state.rotate;
+		this.setState({
+			rotate: oldRotate - 90
+		});
+	}
+
+	rotateRight = () => {
+		const oldRotate = this.state.rotate;
+		this.setState({
+			rotate: oldRotate + 90
+		});
 	}
 
 	render() {
@@ -139,7 +164,7 @@ class ProfilePicModal extends Component {
 				</Modal.Body>
 				<Modal.Footer>
 					<Button onClick={this.handleUpload}>
-						Upload
+						Save and Close
 					</Button>
 				</Modal.Footer>
 			</Modal>
