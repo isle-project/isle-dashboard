5e667c7c8fa0d28fbd1861350b8d238fdbf4dd2c
Planeshifter <pburckhardt@outlook.com>
2020-01-31 14:15:28 -0500

    Add license headers

--- src/constants/action_types.js
+++ src/constants/action_types.js
@@ -1,3 +1,20 @@
+/**
+* Copyright (C) 2016-2020 The ISLE Authors
+*
+* The isle-dashboard program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 export const ADD_ENROLLED_NAMESPACE = 'ADD_ENROLLED_NAMESPACE';
 export const ADD_NOTIFICATION = 'ADD_NOTIFICATION';
 export const APPEND_CREATED_NAMESPACE = 'APPEND_CREATED_NAMESPACE';
