921bb32e49adcfcd3b594feacfc2f1da727bdfaa
Planeshifter <pburckhardt@outlook.com>
2020-01-31 14:15:28 -0500

    Add license headers

--- src/components/async/index.js
+++ src/components/async/index.js
@@ -1,6 +1,24 @@
+/**
+* Copyright (C) 2016-2020 The ISLE Authors
+*
+* The isle-dashboard program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // MODULES //
 
 import React, { Component } from 'react';
+import Spinner from './spinner.js';
 
 
 // MAIN //
@@ -23,19 +41,8 @@ function asyncComponent( importComponent ) {
 		}
 
 		render() {
-			const spinner = <div className="sk-cube-grid">
-				<div className="sk-cube sk-cube1"></div>
-				<div className="sk-cube sk-cube2"></div>
-				<div className="sk-cube sk-cube3"></div>
-				<div className="sk-cube sk-cube4"></div>
-				<div className="sk-cube sk-cube5"></div>
-				<div className="sk-cube sk-cube6"></div>
-				<div className="sk-cube sk-cube7"></div>
-				<div className="sk-cube sk-cube8"></div>
-				<div className="sk-cube sk-cube9"></div>
-			</div>;
 			const C = this.state.component;
-			return C ? <C {...this.props} /> : spinner;
+			return C ? <C {...this.props} /> : <Spinner />;
 		}
 	}
 	return AsyncComponent;
---
+++ src/components/async/spinner.js
@@ -0,0 +1,43 @@
+/**
+* The MIT License (MIT)
+*
+* Copyright (c) 2015 Tobias Ahlin
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// MAIN //
+
+const Spinner = () => {
+	return ( <div className="sk-cube-grid">
+		<div className="sk-cube sk-cube1"></div>
+		<div className="sk-cube sk-cube2"></div>
+		<div className="sk-cube sk-cube3"></div>
+		<div className="sk-cube sk-cube4"></div>
+		<div className="sk-cube sk-cube5"></div>
+		<div className="sk-cube sk-cube6"></div>
+		<div className="sk-cube sk-cube7"></div>
+		<div className="sk-cube sk-cube8"></div>
+		<div className="sk-cube sk-cube9"></div>
+	</div> );
+};
+
+
+// EXPORTS //
+
+export default Spinner;
