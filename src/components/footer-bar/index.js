796ec77d092ee8d86d7e81fb29fbec6d0f8dbd99
Planeshifter <pburckhardt@outlook.com>
2020-05-16 16:38:11 -0400

    Add footer bar

---
+++ src/components/footer-bar/index.js
@@ -0,0 +1,49 @@
+/**
+* Copyright (C) 2016-present The ISLE Authors
+*
+* The isle-dashboard program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// MODULES //
+
+import React, { Component } from 'react';
+import { withTranslation } from 'react-i18next';
+import LanguageSwitcher from 'components/language-switcher';
+import './footer_bar.css';
+
+
+// MAIN //
+
+class FooterBar extends Component {
+	render() {
+		return ( <div className="footer-bar" >
+			<div className="isle-logo" >
+				<img src="img/isle_logo.svg" alt="ISLE Logo" />
+				<div className="footer-bar-copyright" >© 2016-2020 The ISLE Authors</div>
+			</div>
+			<div className="isle-terms" >
+				<a target="_blank" href="" >Terms</a>
+				<a target="_blank" href="" >Privacy</a>
+			</div>
+			<div>
+			</div>
+			<LanguageSwitcher />
+		</div> );
+	}
+}
+
+
+// EXPORTS //
+
+export default withTranslation( [ 'footer_bar', 'common' ] )( FooterBar );
