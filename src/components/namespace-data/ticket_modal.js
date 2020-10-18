fec61e39a0bbd6fb629a6bb60bcdd6d0641a4a83
Planeshifter <pburckhardt@outlook.com>
2020-10-17 20:33:35 -0400

    Populate ticket page on namespace data

---
+++ src/components/namespace-data/ticket_modal.js
@@ -0,0 +1,143 @@
+/**
+* Copyright (C) 2016-present The ISLE Authors
+*
+* The isle-dashboard program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// MODULES //
+
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Modal from 'react-bootstrap/Modal';
+import FormLabel from 'react-bootstrap/FormLabel';
+import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import logger from 'debug';
+import server from 'constants/server';
+
+
+// FUNCTIONS //
+
+const createTooltip = ( str ) => {
+	return <Tooltip id="tooltip">{str}</Tooltip>;
+};
+
+
+// VARIABLES //
+
+const debug = logger( 'isle-dashboard' );
+
+
+// MAIN //
+
+class TicketModal extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			response: ''
+		};
+	}
+
+	handleResponseChange = ( event ) => {
+		this.setState({
+			response: event.target.value
+		});
+	}
+
+	renderMessage = ( val, idx ) => {
+		return 'MESSAGES';
+	}
+
+	render() {
+		const t = this.props.t;
+		console.log( this.props.ticket );
+		return (
+			<Fragment>
+				<Modal show={this.props.show} onHide={this.props.onHide} dialogClassName="modal-60w" >
+					<Modal.Header>
+						<Modal.Title as="h3" style={{ width: '100%' }}>
+							<img className="ticket-modal-pic" src={`${server}/thumbnail/${this.props.ticket.user.picture}`} alt="Profile Pic" />
+							{this.props.ticket.title}
+							<OverlayTrigger placement="right" overlay={createTooltip( t('close-ticket-modal') )}>
+								<Button
+									onClick={this.props.onHide}
+									style={{ float: 'right' }}
+									variant="outline-secondary"
+									aria-label={t('close-ticket-modal')}
+								>
+									<span aria-hidden="true">x</span>
+								</Button>
+							</OverlayTrigger>
+						</Modal.Title>
+					</Modal.Header>
+					<Modal.Body style={{ height: '60vh', overflowY: 'auto' }}>
+						<span className="title">Description:</span>
+						<p style={{ whiteSpace: 'pre-wrap' }}>
+							{this.props.ticket.description}
+						</p>
+						<hr />
+						<span className="title">Messages:</span>
+						{this.props.ticket.messages.map( this.renderMessage )}
+						<hr />
+					</Modal.Body>
+					<Modal.Footer style={{ background: 'ghostwhite' }} >
+						<FormGroup controlId="form-text" style={{ width: 'calc(100% - 200px)' }} >
+							<FormLabel>{t('enter-response')}</FormLabel>
+							<FormControl
+								name="body"
+								type="text"
+								as="textarea" rows="5"
+								value={this.state.response}
+								onChange={this.handleResponseChange}
+							>
+							</FormControl>
+						</FormGroup>
+						<div style={{ float: 'right', width: '180px' }} >
+							<Button block >
+								Submit Response
+							</Button>
+							<Button block >
+								Close Ticket
+							</Button>
+							<Button onClick={this.props.onHide} block >
+								Hide
+							</Button>
+						</div>
+					</Modal.Footer>
+				</Modal>
+			</Fragment>
+		);
+	}
+}
+
+
+// PROPERTIES //
+
+TicketModal.propTypes = {
+	onHide: PropTypes.func.isRequired,
+	show: PropTypes.bool.isRequired,
+	ticket: PropTypes.object.isRequired,
+	t: PropTypes.func.isRequired
+};
+
+TicketModal.defaultProps = {};
+
+
+// EXPORTS
+
+export default TicketModal;
--- src/components/namespace-data/tickets_page.js
+++ src/components/namespace-data/tickets_page.js
@@ -22,10 +22,22 @@ import PropTypes from 'prop-types';
 import logger from 'debug';
 import { withTranslation } from 'react-i18next';
 import ReactTable from 'react-table';
+import moment from 'moment';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import PINF from '@stdlib/constants/math/float64-pinf';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import createBooleanColumn from 'components/admin/create_boolean_column.js';
+import createTextColumn from 'components/admin/create_text_column.js';
+import createDateColumn from 'components/admin/create_date_column.js';
+import server from 'constants/server';
+import TicketModal from './ticket_modal.js';
 import 'react-table/react-table.css';
 import 'css/table.css';
 import 'css/input_range.css';
-import './files_page.css';
+import './tickets_page.css';
 
 
 // VARIABLES //
@@ -38,18 +50,143 @@ const debug = logger( 'isle:files-page' );
 class TicketsPage extends Component {
 	constructor( props ) {
 		super( props );
+
+		this.state = {
+			showTicketModal: false,
+			selectedTicket: null
+		};
 		this.columns = this.createColumns();
 	}
 
 	componentDidMount() {
 		debug( 'Component did mount...' );
-		console.log( this.props.namespace )
 		this.props.getCourseTickets( this.props.namespace._id );
 	}
 
-	createColumns = () => {
-		return [
+	toggleTicketModal = () => {
+		this.setState({
+			showTicketModal: !this.state.showTicketModal
+		});
+	}
 
+	createColumns = () => {
+		const { t } = this.props;
+		const tickets = this.props.tickets || [];
+		let minTime = PINF;
+		let maxTime = 0;
+		for ( let i = 0; i < tickets.length; i++ ) {
+			if ( tickets[ i ].createdAt > maxTime ) {
+				maxTime = tickets[ i ].createdAt;
+			}
+			if ( tickets[ i ].updatedAt > maxTime ) {
+				maxTime = tickets[ i ].updatedAt;
+			}
+			if ( tickets[ i ].createdAt < minTime ) {
+				minTime = tickets[ i ].createdAt;
+			}
+			if ( tickets[ i ].updatedAt < minTime ) {
+				minTime = tickets[ i ].updatedAt;
+			}
+		}
+		maxTime = moment( maxTime );
+		minTime = moment( minTime );
+		return [
+			{
+				Header: 'Pic',
+				accessor: 'user.picture',
+				Cell: ( row ) => {
+					return <img className="table-pic" src={`${server}/thumbnail/${row.value}`} alt="Profile Pic" />;
+				},
+				style: { marginTop: '8px', color: 'darkslategrey' },
+				maxWidth: 46,
+				minWidth: 46,
+				filterable: false,
+				resizable: false,
+				sortable: false
+			},
+			createTextColumn({
+				Header: t('common:name'),
+				accessor: 'user.name',
+				style: { marginTop: '8px', color: 'darkslategrey' }
+			}),
+			createTextColumn({
+				Header: t('common:email'),
+				accessor: 'user.email',
+				style: { marginTop: '8px', color: 'darkslategrey' }
+			}),
+			createTextColumn({
+				Header: t('common:title'),
+				accessor: 'title',
+				minWidth: 200
+			}),
+			{
+				Header: t('common:description'),
+				accessor: 'description',
+				Cell: ( row ) => {
+					return ( <OverlayTrigger placement="top" overlay={<Card className="tickets-description-overlay" body id="description-tooltip">
+						{row.value}
+					</Card>}>
+						<p className="tickets-description" >{row.value}</p>
+					</OverlayTrigger> );
+				},
+				style: { marginTop: '8px', color: 'darkslategrey' },
+				minWidth: 350
+			},
+			{
+				Header: 'Reply',
+				accessor: '_id',
+				Cell: ( row ) => {
+					return (
+						<OverlayTrigger placement="left" overlay={<Tooltip id="open-ticket-tooltip">{t('open-ticket-tooltip')}</Tooltip>}>
+							<Button
+								size="sm" variant="outline-secondary"
+								onClick={() => {
+									this.setState({
+										selectedTicket: row.original
+									}, this.toggleTicketModal );
+								}} >
+								<i className="fas fa-reply"></i>
+							</Button>
+						</OverlayTrigger>
+					);
+				},
+				resizable: false,
+				filterable: false,
+				sortable: false,
+				width: 45
+			},
+			createTextColumn({
+				Header: t('common:lesson'),
+				accessor: 'lesson.title',
+				maxWidth: 140,
+				Cell: ( row ) => {
+					if ( !row.value ) {
+						return null;
+					}
+					const url = `${server}/${this.props.namespace.title}/${row.value}`;
+					return (
+						<OverlayTrigger placement="right" overlay={<Tooltip id="open-lesson-tooltip">{t('namespace_data:open-lesson-new-tab')}</Tooltip>}>
+							<Badge variant="light" style={{ fontSize: '1em' }} >
+								<a href={url} target="_blank">
+									{row.value}
+								</a>
+							</Badge>
+						</OverlayTrigger>
+					);
+				}
+			}),
+			createBooleanColumn({
+				Header: t('common:done'),
+				accessor: 'done',
+				trueLabel: t('common:done'),
+				falseLabel: t('common:not-done')
+			}),
+			createDateColumn({
+				Header: t('created-at'),
+				accessor: 'createdAt',
+				startDate: minTime,
+				endDate: maxTime
+			})
 		];
 	}
 
@@ -63,6 +200,12 @@ class TicketsPage extends Component {
 					this.reactTable = r;
 				}}
 			/>
+			{ this.state.showTicketModal ? <TicketModal
+				t={this.props.t}
+				show={this.state.showTicketModal}
+				onHide={this.toggleTicketModal}
+				ticket={this.state.selectedTicket}
+			/> : null }
 		</div> );
 	}
 }
