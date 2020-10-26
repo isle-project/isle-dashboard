57519f0c082c73ec1b3dfdecda1e6f638bd29ee2
Planeshifter <pburckhardt@outlook.com>
2020-10-26 11:27:43 -0400

    Reorder how form data are attached

--- src/components/namespace-data/index.js
+++ src/components/namespace-data/index.js
@@ -86,9 +86,9 @@ class NamespaceData extends Component {
 		debug( 'Uploading file...' );
 		const file = event.target.files[ 0 ];
 		const formData = new FormData();
+		formData.append( 'file', file, file.name );
 		formData.append( 'namespaceName', this.props.namespace.title );
 		formData.append( 'owner', 'true' );
-		formData.append( 'file', file, file.name );
 		this.props.uploadFile({
 			formData: formData
 		});
