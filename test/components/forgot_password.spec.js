7cc67b8234d67c2898268a4b8038fc02845bdf05
Planeshifter <pburckhardt@outlook.com>
2020-11-09 16:40:30 -0500

    Add tests

---
+++ test/components/forgot_password.spec.js
@@ -0,0 +1,59 @@
+/**
+* Copyright (C) 2016-present The ISLE Authors
+*
+* The isle-dashboard program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// MODULES //
+
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import noop from '@stdlib/utils/noop';
+import ForgotPassword from '../../src/components/forgot-password';
+
+
+// TESTS //
+
+describe( '<ForgotPassword />', function test() {
+	const history = createMemoryHistory();
+
+	it( 'should render without throwing an error', () => {
+		render(
+			<Router history={history}>
+				<ForgotPassword forgotPassword={noop} />
+			</Router>
+		);
+		expect( screen.getByRole( 'heading' ) ).toHaveTextContent( 'common:forgot-password' );
+	});
+
+	it( 'should update email field on change', () => {
+		const { queryByPlaceholderText } = render(
+			<Router history={history}>
+				<ForgotPassword forgotPassword={noop} />
+			</Router>
+		);
+		const emailInput = queryByPlaceholderText( 'common:enter-email' );
+		const expected = 'isle@stat.cmu.edu';
+		const event = {
+			target: {
+				value: expected
+			}
+		};
+		fireEvent.change( emailInput, event );
+		expect( emailInput.value ).toBe( expected );
+	});
+});
---
+++ test/components/signup.spec.js
@@ -0,0 +1,110 @@
+/**
+* Copyright (C) 2016-present The ISLE Authors
+*
+* The isle-dashboard program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// MODULES //
+
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import noop from '@stdlib/utils/noop';
+import Signup from './../../src/components/signup';
+
+
+// TESTS //
+
+describe( '<Signup />', function test() {
+	const history = createMemoryHistory();
+
+	it( 'should render without throwing an error', () => {
+		render(
+			<Router history={history}>
+				<Signup createUser={noop} />
+			</Router>
+		);
+		expect( screen.getByRole( 'heading' ) ).toHaveTextContent( 'Dashboard' );
+	});
+
+	it( 'should update email field on change', () => {
+		const { queryByPlaceholderText } = render(
+			<Router history={history}>
+				<Signup createUser={noop} />
+			</Router>
+		);
+		const emailInput = queryByPlaceholderText( 'common:enter-email' );
+		const expected = 'isle@stat.cmu.edu';
+		const event = {
+			target: {
+				value: expected
+			}
+		};
+		fireEvent.change( emailInput, event );
+		expect( emailInput.value ).toBe( expected );
+	});
+
+	it( 'should update name field on change', () => {
+		const { queryByPlaceholderText } = render(
+			<Router history={history}>
+				<Signup createUser={noop} />
+			</Router>
+		);
+		const nameInput = queryByPlaceholderText( 'enter-name' );
+		const expected = 'ISLE User';
+		const event = {
+			target: {
+				value: expected
+			}
+		};
+		fireEvent.change( nameInput, event );
+		expect( nameInput.value ).toBe( expected );
+	});
+
+	it( 'should update password field on change', () => {
+		const { queryByPlaceholderText } = render(
+			<Router history={history}>
+				<Signup createUser={noop} />
+			</Router>
+		);
+		const passwordInput = queryByPlaceholderText( 'choose-password' );
+		const expected = 'birthday';
+		const event = {
+			target: {
+				value: expected
+			}
+		};
+		fireEvent.change( passwordInput, event );
+		expect( passwordInput.value ).toBe( expected );
+	});
+
+	it( 'should update password confirmation field on change', () => {
+		const { queryByPlaceholderText } = render(
+			<Router history={history}>
+				<Signup createUser={noop} />
+			</Router>
+		);
+		const passwordInput = queryByPlaceholderText( 'confirm-password' );
+		const expected = 'birthday';
+		const event = {
+			target: {
+				value: expected
+			}
+		};
+		fireEvent.change( passwordInput, event );
+		expect( passwordInput.value ).toBe( expected );
+	});
+});
