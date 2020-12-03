b115cd483110b8805db07a58d2cecb7ab7231437
Planeshifter <pburckhardt@outlook.com>
2020-12-03 13:04:14 -0500

    Load component lazily

--- src/ev/components/admin/data-explorer/index.js
+++ src/ev/components/admin/data-explorer/index.js
@@ -1,40 +1,11 @@
 // MODULES //
 
-import Button from 'react-bootstrap/Button';
-import DataExplorer from '@isle-project/components/data-explorer';
-import SessionContext from '@isle-project/session/context.js';
-import Session from '@isle-project/session';
+import asyncComponent from 'components/async';
 
 
 // MAIN //
 
-const AdminDataExplorer = ( props ) => {
-	const keys = Object.keys( props.data );
-	const quantitative = props.quantitative || keys;
-	const categorical = props.categorical || keys;
-	const session = new Session({}, true );
-	return (
-		<div className="admin-outer-container" >
-			<h2>
-				{props.title}
-				<Button onClick={props.close} style={{ float: 'right' }} >Cancel</Button>
-			</h2>
-			<SessionContext.Provider value={session} >
-				<div className="Lesson">
-					<DataExplorer
-						editor={false}
-						data={props.data}
-						quantitative={quantitative}
-						categorical={categorical}
-						style={{
-							height: '74vh'
-						}}
-					/>
-				</div>
-			</SessionContext.Provider>
-		</div>
-	);
-};
+const AdminDataExplorer = asyncComponent( () => import( './main.js' ) );
 
 
 // EXPORTS //
