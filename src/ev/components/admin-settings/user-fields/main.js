4440f164046dddeaaa03eff9373ce50a353b3638
Planeshifter <pburckhardt@outlook.com>
2020-12-08 02:20:25 -0500

    Display list of custom fields

--- src/ev/components/admin-settings/user-fields/main.js
+++ src/ev/components/admin-settings/user-fields/main.js
@@ -4,8 +4,13 @@ import React, { Component, Fragment } from 'react';
 import { withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import ListGroup from 'react-bootstrap/ListGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import LicenseBarrier from 'ev/components/admin/barrier';
 import CreateFieldModal from './create_field_modal.js';
+import './user_fields.css';
 
 
 // MAIN //
@@ -29,25 +34,71 @@ class AdminSettingsUserFields extends Component {
 		});
 	}
 
+	handleDeletionFactory = ( id ) => {
+		return () => {
+			this.props.deleteCustomField( id );
+		};
+	}
+
 	render() {
+		const { t } = this.props;
 		return (
 			<Fragment>
 				<div className="admin-settings-outer-container" >
 					<LicenseBarrier license={this.props.admin.license} >
 						<h1>{this.props.t('user-fields')}</h1>
 						<p>{this.props.t('user-fields-description')}</p>
+						{ this.props.user.customFields.length > 0 ? <ListGroup className="admin-settings-fields-list" >
+							{this.props.user.customFields.map( ( field, idx ) => {
+								return (
+									<ListGroup.Item key={idx} >
+										<b>{field.name}</b>: {field.description} (type: {field.type})
+										<OverlayTrigger placement="left" overlay={<Tooltip id="delete_field">{t('delete-field')}</Tooltip>}>
+											<Button
+												variant="outline-secondary"
+												onClick={this.handleDeletionFactory( field._id )}
+												aria-label={t('delete-field')}
+											>
+												<div className="fa fa-trash-alt" />
+											</Button>
+										</OverlayTrigger>
+										<Button
+											variant="outline-secondary"
+											onClick={() => {
+												this.props.incrementFieldPosition( field._id );
+											}}
+											disabled={idx === this.props.user.customFields.length - 1}
+										>
+											<i className="fas fa-arrow-down"></i>
+										</Button>
+										<Button
+											variant="outline-secondary"
+											onClick={() => {
+												this.props.decrementFieldPosition( field._id );
+											}}
+											disabled={idx === 0}
+										>
+											<i className="fas fa-arrow-up"></i>
+										</Button>
+									</ListGroup.Item>
+								);
+							})}
+						</ListGroup> : <Jumbotron className="admin-settings-fields-jumbotron" >
+							{t('no-custom-fields')}
+						</Jumbotron> }
 						<Button variant="primary" onClick={this.toggleCreateModal} >
 							<i className="fas fa-plus" style={{ marginRight: 8 }}></i>
 							{this.props.t('add-user-field')}
 						</Button>
 					</LicenseBarrier>
 				</div>
-				<CreateFieldModal
+				{this.state.showCreateModal ? <CreateFieldModal
 					show={this.state.showCreateModal}
 					onHide={this.toggleCreateModal}
 					t={this.props.t}
 					createCustomField={this.props.createCustomField}
-				/>
+					customFields={this.props.customFields}
+				/> : null}
 			</Fragment>
 		);
 	}
@@ -59,11 +110,15 @@ class AdminSettingsUserFields extends Component {
 AdminSettingsUserFields.propTypes = {
 	admin: PropTypes.object,
 	createCustomField: PropTypes.func.isRequired,
-	getCustomFields: PropTypes.func.isRequired
+	customFields: PropTypes.array,
+	getCustomFields: PropTypes.func.isRequired,
+	user: PropTypes.object
 };
 
 AdminSettingsUserFields.defaultProps = {
-	admin: {}
+	admin: {},
+	customFields: [],
+	user: {}
 };
 
 
