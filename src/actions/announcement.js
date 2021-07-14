771b06a90dedfdb775b17ea4cc318d04b98a679d
Planeshifter <pburckhardt@outlook.com>
2021-07-14 17:03:47 -0400

    Add JSdoc comments

--- src/actions/announcement.js
+++ src/actions/announcement.js
@@ -25,6 +25,13 @@ import { namespaceAxios } from 'helpers/axios.js';
 
 // EXPORTS //
 
+/**
+ * Returns an action object to be used in signalling that an announcement has been edited.
+ *
+ * @param {Object} announcement
+ * @param {string} namespaceName
+ * @returns {Object} an action object
+ */
 export function editedAnnouncement( announcement, namespaceName ) {
 	return {
 		type: EDITED_ANNOUNCEMENT,
@@ -35,6 +42,13 @@ export function editedAnnouncement( announcement, namespaceName ) {
 	};
 }
 
+/**
+ * Returns an action object to be used in signalling that an announcement has been created.
+ *
+ * @param {Object} announcement
+ * @param {string} namespaceName
+ * @returns {Object} an action object
+ */
 export function createdAnnouncement( announcement, namespaceName ) {
 	return {
 		type: CREATED_ANNOUNCEMENT,
@@ -45,6 +59,13 @@ export function createdAnnouncement( announcement, namespaceName ) {
 	};
 }
 
+/**
+ * Returns an action object to be used in signalling that an announcement has been deleted.
+ *
+ * @param {number} index - index of the announcement
+ * @param {string} namespaceName
+ * @returns {Object} an action object
+ */
 export function deletedAnnouncement( index, namespaceName ) {
 	return {
 		type: DELETED_ANNOUNCEMENT,
@@ -55,6 +76,14 @@ export function deletedAnnouncement( index, namespaceName ) {
 	};
 }
 
+/**
+ * Makes a request to create an announcement.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {Object} opts - request options
+ * @param {string} opts.namespaceName - namespace name
+ * @param {Object} opts.announcement - announcement object
+ */
 export const addAnnouncement = async ( dispatch, { namespaceName, announcement }) => {
 	try {
 		const res = await namespaceAxios.post( server+'/new_announcement', {
@@ -71,12 +100,27 @@ export const addAnnouncement = async ( dispatch, { namespaceName, announcement }
 	}
 };
 
+/**
+ * Returns a function for making an request to create an announcement with a bound dispatch function
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} wrapped `addAnnouncement` function
+ */
 export const addAnnouncementInjector = dispatch => {
 	return async ( { namespaceName, announcement } ) => {
 		await addAnnouncement( dispatch, { namespaceName, announcement } );
 	};
 };
 
+/**
+ * Makes a request to delete an announcement.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {Object} opts - request options
+ * @param {string} opts.namespaceName - namespace name
+ * @param {number} opts.index - index of the announcement
+ * @param {string} opts.createdAt - creation time of the announcement
+ */
 export const deleteAnnouncement = async ( dispatch, { namespaceName, createdAt, index }) => {
 	try {
 		const res = await namespaceAxios.post( server+'/delete_announcement', {
@@ -93,12 +137,26 @@ export const deleteAnnouncement = async ( dispatch, { namespaceName, createdAt, 
 	}
 };
 
+/**
+ * Returns a function for making an request to delete an announcement with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} wrapped `deleteAnnouncement` function
+ */
 export const deleteAnnouncementInjector = dispatch => {
 	return async ( { namespaceName, createdAt, index } ) => {
 		await deleteAnnouncement( dispatch, { namespaceName, createdAt, index } );
 	};
 };
 
+/**
+ * Makes a request to edit an announcement.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {Object} opts - request options
+ * @param {string} opts.namespaceName - namespace name
+ * @param {Object} opts.announcement - announcement object
+ */
 export const editAnnouncement = async ( dispatch, { namespaceName, announcement }) => {
 	try {
 		const res = await namespaceAxios.post( server+'/edit_announcement', {
@@ -115,6 +173,12 @@ export const editAnnouncement = async ( dispatch, { namespaceName, announcement 
 	}
 };
 
+/**
+ * Returns a function for making an request to edit an announcement with a bound dispatch function.
+ *
+* @param {Function} dispatch - dispatch function
+* @returns {Function} wrapped `editAnnouncement` function
+*/
 export const editAnnouncementInjector = dispatch => {
 	return async ( { namespaceName, announcement } ) => {
 		await editAnnouncement( dispatch, { namespaceName, announcement } );
