049eca4dc8d891d2f43aaa5d4d0f7254f421212a
Planeshifter <pburckhardt@outlook.com>
2021-07-14 17:55:14 -0400

    Add JSDoc comments

--- src/actions/statistics.js
+++ src/actions/statistics.js
@@ -26,6 +26,12 @@ import { GET_OVERVIEW_STATISTICS, GET_HISTORICAL_OVERVIEW_STATISTICS, GET_REQUES
 
 // EXPORTS //
 
+/**
+ * Makes a GET request to the server for the overview statistics of the dashboard showing current metrics of the ISLE instance.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {void}
+ */
 export const getOverviewStatistics = async ( dispatch ) => {
 	try {
 		const res = await axios.get( server+'/admin_overview_statistics' );
@@ -40,12 +46,24 @@ export const getOverviewStatistics = async ( dispatch ) => {
 	}
 };
 
+/**
+ * Returns a function for making a GET request to the server for overview statistics with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function for making a GET request to the server for overview statistics
+ */
 export const getOverviewStatisticsInjector = ( dispatch ) => {
 	return async () => {
 		await getOverviewStatistics( dispatch );
 	};
 };
 
+/**
+ * Makes a GET request to the server for the historical overview statistics of the dashboard showing metrics of the ISLE instance overa period of time.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {void}
+ */
 export const getHistoricalOverviewStats = async ( dispatch ) => {
 	try {
 		const res = await axios.get( server+'/admin_historical_overview_statistics' );
@@ -60,12 +78,24 @@ export const getHistoricalOverviewStats = async ( dispatch ) => {
 	}
 };
 
+/**
+ * Returns a function for making a GET request to the server for historical overview statistics with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function for making a GET request to the server for historical overview statistics
+ */
 export const getHistoricalOverviewStatsInjector = ( dispatch ) => {
 	return async () => {
 		await getHistoricalOverviewStats( dispatch );
 	};
 };
 
+/**
+ * Makes a GET request to the server for request statistics with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {void}
+ */
 export const getRequestStatistics = async ( dispatch ) => {
 	try {
 		const res = await axios.get( server+'/admin_request_statistics' );
@@ -80,6 +110,12 @@ export const getRequestStatistics = async ( dispatch ) => {
 	}
 };
 
+/**
+ * Returns a function for making a GET request to the server for request statistics with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function for making a GET request to the server for request statistics
+ */
 export const getRequestStatisticsInjector = ( dispatch ) => {
 	return async () => {
 		await getRequestStatistics( dispatch );
