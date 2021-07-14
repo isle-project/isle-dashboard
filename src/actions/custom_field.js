c5f96a6bb742e2751cc5667fdb6f3c3c9de6cc8f
Planeshifter <pburckhardt@outlook.com>
2021-07-14 18:32:58 -0400

    Add JSDoc

--- src/actions/custom_field.js
+++ src/actions/custom_field.js
@@ -26,6 +26,12 @@ import { globalAxios } from 'helpers/axios.js';
 
 // EXPORTS //
 
+/**
+ * Makes a GET request to the server to get all custom user fields.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {void}
+ */
 export const getCustomFields = async ( dispatch ) => {
 	try {
 		const res = await globalAxios.get( server+'/get_custom_fields' );
@@ -40,12 +46,25 @@ export const getCustomFields = async ( dispatch ) => {
 	}
 };
 
+/**
+ * Returns a function to make a GET request to the server to get all custom user fields with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function to make a GET request to the server to get all custom user fields
+ */
 export const getCustomFieldsInjector = dispatch => {
 	return async () => {
 		await getCustomFields( dispatch );
 	};
 };
 
+/**
+ * Makes a POST request to the server to increment a custom field's position.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {string} id - the custom field's unique id
+ * @returns {void}
+ */
 export const incrementFieldPosition = async ( dispatch, id ) => {
 	try {
 		const res = await globalAxios.post( server+'/increment_field_position', {
@@ -62,12 +81,25 @@ export const incrementFieldPosition = async ( dispatch, id ) => {
 	}
 };
 
+/**
+ * Returns a function to make a POST request to the server to increment a custom field's position with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function to make a POST request to the server to increment a custom field's position
+ */
 export const incrementFieldPositionInjector = dispatch => {
 	return async ( id ) => {
 		await incrementFieldPosition( dispatch, id );
 	};
 };
 
+/**
+ * Makes a POST request to the server to decrement a custom field's position.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {string} id - the custom field's unique id
+ * @returns {void}
+ */
 export const decrementFieldPosition = async ( dispatch, id ) => {
 	try {
 		const res = await globalAxios.post( server+'/decrement_field_position', {
@@ -84,12 +116,25 @@ export const decrementFieldPosition = async ( dispatch, id ) => {
 	}
 };
 
+/**
+ * Returns a function to make a POST request to the server to decrement a custom field's position with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function to make a POST request to the server to decrement a custom field's position
+ */
 export const decrementFieldPositionInjector = dispatch => {
 	return async ( id ) => {
 		await decrementFieldPosition( dispatch, id );
 	};
 };
 
+/**
+ * Makes a POST request to the server to delete a custom field.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {string} id - the custom field's unique id
+ * @returns {void}
+ */
 export const deleteCustomField = async ( dispatch, id ) => {
 	try {
 		const res = await globalAxios.post( server+'/delete_custom_field', {
@@ -111,12 +156,33 @@ export const deleteCustomField = async ( dispatch, id ) => {
 	}
 };
 
+/**
+ * Returns a function to make a POST request to the server to delete a custom field with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function to make a POST request to the server to delete a custom field
+ */
 export const deleteCustomFieldInjector = dispatch => {
 	return async ( id ) => {
 		await deleteCustomField( dispatch, id );
 	};
 };
 
+/**
+ * Makes a POST request to the server to create a custom field.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {Object} field - the custom field's data
+ * @param {string} field.name - the custom field's name
+ * @param {string} field.description - the custom field's description
+ * @param {string} field.type - the custom field's type
+ * @param {Array} field.options - the custom field's options (if type is `dropdown`)
+ * @param {number} field.position - the custom field's position
+ * @param {boolean} field.showOnProfile - whether the custom field should be shown on the users' profile
+ * @param {boolean} field.editableOnSignup - whether the custom field should be editable on signup
+ * @param {boolean} field.editableOnProfile - whether the custom field should be editable on the users' profile
+ * @returns {void}
+ */
 export const createCustomField = async ( dispatch, {
 	name,
 	description,
@@ -153,6 +219,12 @@ export const createCustomField = async ( dispatch, {
 	}
 };
 
+/**
+ * Returns a function to make a POST request to the server to create a custom field with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function to make a POST request to the server to create a custom field
+ */
 export const createCustomFieldInjector = dispatch => {
 	return async ({
 		name,
@@ -177,6 +249,20 @@ export const createCustomFieldInjector = dispatch => {
 	};
 };
 
+/**
+ * Makes a POST request to the server to update a custom field.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @param {Object} field - the custom field's data
+ * @param {string} field.name - the custom field's name
+ * @param {string} field.description - the custom field's description
+ * @param {string} field.type - the custom field's type
+ * @param {Array} field.options - the custom field's options (if type is `dropdown`)
+ * @param {number} field.position - the custom field's position
+ * @param {boolean} field.showOnProfile - whether the custom field should be shown on the users' profile
+ * @param {boolean} field.editableOnSignup - whether the custom field should be editable on signup
+ * @param {boolean} field.editableOnProfile - whether the custom field should be editable on the users' profile
+ */
 export const updateCustomField = async ( dispatch, {
 	name,
 	description,
@@ -221,6 +307,12 @@ export const updateCustomField = async ( dispatch, {
 	}
 };
 
+/**
+ * Returns a function to make a POST request to the server to update a custom field with a bound dispatch function.
+ *
+ * @param {Function} dispatch - dispatch function
+ * @returns {Function} a function to make a POST request to the server to update a custom field
+ */
 export const updateCustomFieldInjector = dispatch => {
 	return async ({
 		name,
