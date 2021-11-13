b9cbfe893bd71232662d1339747c0c156b340428
Planeshifter <pburckhardt@outlook.com>
2021-11-13 10:27:58 -0500

    Rewrite using hooks

--- src/components/confirm-email/index.js
+++ src/components/confirm-email/index.js
@@ -17,9 +17,9 @@
 
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import queryString from 'query-string';
@@ -28,53 +28,49 @@ import 'css/login.css';
 
 // MAIN //
 
-class ConfirmEmail extends Component {
-	constructor( props ) {
-		super( props );
+/**
+ * Renders a confirmation email page that is displayed when a user clicks on a confirmation link in their email.
+ *
+ * @param {Object} props - component props
+ * @param {Function} props.confirmEmail - function to confirm a user's email address
+ * @param {Object} props.settings - ISLE instance settings
+ * @returns {ReactElement} component
+ */
+const ConfirmEmail = ({ confirmEmail, settings }) => {
+	const { t } = useTranslation( 'common' );
+	const [ message, setMessage ] = useState( '' );
 
-		this.state = {
-			message: 'common:waiting'
-		};
-	}
-
-	async componentDidMount() {
+	useEffect( () => {
 		const hash = window.location.hash.substring( 16 );
 		const qs = queryString.parse( hash );
 		const token = qs[ 'token' ];
-		const newMessage = await this.props.confirmEmail( token );
-
-		// eslint-disable-next-line react/no-did-mount-set-state
-		this.setState({
-			message: newMessage
+		confirmEmail( token ).then( ( newMessage ) => {
+			setMessage( newMessage );
 		});
-	}
-
-	render() {
-		const { t, settings } = this.props;
-		return (
-			<div className="login">
-				<Card style={{ boxShadow: '0 0 8px rgba(0,0,0,0.3)', borderRadius: '6px', opacity: 0.98, background: 'rgba(255,255,255,0.75)' }}>
-					<Card.Header>
-						<Card.Title as="h3">{t('common:confirm-email')}</Card.Title>
-					</Card.Header>
-					<Card.Body>
-						<p data-testid="message" >{this.state.message}</p>
-					</Card.Body>
-					<Card.Footer style={{ background: 'rgba(255,255,255,0.6)', textAlign: 'right' }}>
-						<Link to="/signup">{t('common:register')}</Link>
-						<span> | </span>
-						<Link to="/login">{t('common:login')}</Link>
-					</Card.Footer>
-				</Card>
-				{settings.brandingLogo ? <img
-					className="login-branding-logo"
-					src={settings.brandingLogo}
-					alt="Branded Logo"
-				/> : null}
-			</div>
-		);
-	}
-}
+	}, [ confirmEmail ] );
+	return (
+		<div className="login">
+			<Card style={{ boxShadow: '0 0 8px rgba(0,0,0,0.3)', borderRadius: '6px', opacity: 0.98, background: 'rgba(255,255,255,0.75)' }}>
+				<Card.Header>
+					<Card.Title as="h3">{t('common:confirm-email')}</Card.Title>
+				</Card.Header>
+				<Card.Body>
+					<p data-testid="message" >{message}</p>
+				</Card.Body>
+				<Card.Footer style={{ background: 'rgba(255,255,255,0.6)', textAlign: 'right' }}>
+					<Link to="/signup">{t('common:register')}</Link>
+					<span> | </span>
+					<Link to="/login">{t('common:login')}</Link>
+				</Card.Footer>
+			</Card>
+			{settings.brandingLogo ? <img
+				className="login-branding-logo"
+				src={settings.brandingLogo}
+				alt="Branded Logo"
+			/> : null}
+		</div>
+	);
+};
 
 
 // PROPERTIES //
@@ -91,4 +87,4 @@ ConfirmEmail.defaultProps = {
 
 // EXPORTS //
 
-export default withTranslation( [ 'common' ] )( ConfirmEmail );
+export default ConfirmEmail;
