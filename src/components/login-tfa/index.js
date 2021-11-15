1b318a2d8e89baf50ad399d859a8aacfd4655020
Planeshifter <pburckhardt@outlook.com>
2021-11-15 11:33:35 -0500

    Rewrite using hooks

--- src/components/login-tfa/index.js
+++ src/components/login-tfa/index.js
@@ -17,9 +17,9 @@
 
 // MODULES //
 
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
@@ -33,88 +33,88 @@ import 'css/login.css';
 
 // MAIN //
 
-class LoginTFA extends Component {
-	constructor( props ) {
-		super( props );
-
-		this.state = {
-			token: ''
-		};
-	}
-
-	handleSubmit = async ( event ) => {
+/**
+ * Renders the login form for two-factor authentication.
+ *
+ * @param {Object} props - component properties
+ * @param {Function} props.handleLoginTFA - callback to handle login
+ * @param {Function} props.fetchCredentials - callback to fetch credentials
+ * @param {Function} props.getEnrollableCohorts - callback to get enrollable cohorts
+ * @param {Object} props.user - user object
+ * @param {Function} props.t - i18n translation function
+ * @param {Object} props.settings - ISLE instance settings
+ * @returns {ReactElement} component
+ */
+const LoginTFA = ({ handleLoginTFA, fetchCredentials, getEnrollableCohorts, user, t, settings }) => {
+	const [ token, setToken ] = useState( '' );
+	const { t } = useTranslation( [ 'login', 'common' ]  );
+	const handleSubmit = async ( event ) => {
 		event.preventDefault();
 		const form = {
-			email: this.props.user.requestTFA.email,
-			password: this.props.user.requestTFA.password,
-			token: this.state.token
+			email: user.requestTFA.email,
+			password: user.requestTFA.password,
+			token
 		};
-		const res = await this.props.handleLoginTFA( form );
+		const res = await handleLoginTFA( form );
 		if ( res ) {
 			const { message, token, id } = res.data;
 			if ( message === 'ok' ) {
-				const user = await this.props.fetchCredentials({ token, id });
+				const user = await fetchCredentials({ token, id });
 				if ( user ) {
-					this.props.getEnrollableCohorts( user );
+					getEnrollableCohorts( user );
 				}
 			}
 		}
 	};
-
-	render() {
-		const { t, settings } = this.props;
-		return (
-			<Fragment>
-				<div className="login">
-					<Card className="login-panel">
-						<Card.Header>
-							<Card.Title as="h1" style={{ textAlign: 'center' }} >
-								ISLE <small>Dashboard</small>
-							</Card.Title>
-						</Card.Header>
-						<Card.Body>
-							<p>
-								{t('enter-tfa-token')}
-							</p>
-							<Form>
-								<FormGroup controlId="form-token">
-									<Row>
-										<Col sm={3}>
-											<FormLabel>{t('token')}</FormLabel>
-										</Col>
-										<Col sm={9}>
-											<FormControl
-												type="text"
-												placeholder={t('enter-token')}
-												onChange={( event ) => {
-													this.setState({
-														token: event.target.value
-													});
-												}}
-											/>
-										</Col>
-									</Row>
-								</FormGroup>
-								<Button
-									className="centered"
-									variant="primary"
-									onClick={this.handleSubmit}
-									type="submit"
-									disabled={this.state.token.length !== 6}
-								>{t('common:login')}</Button>
-							</Form>
-						</Card.Body>
-					</Card>
-					{settings.brandingLogo ? <img
-						className="login-branding-logo"
-						src={settings.brandingLogo}
-						alt="Branded Logo"
-					/> : null}
-				</div>
-			</Fragment>
-		);
-	}
-}
+	return (
+		<Fragment>
+			<div className="login">
+				<Card className="login-panel">
+					<Card.Header>
+						<Card.Title as="h1" style={{ textAlign: 'center' }} >
+							ISLE <small>Dashboard</small>
+						</Card.Title>
+					</Card.Header>
+					<Card.Body>
+						<p>
+							{t('enter-tfa-token')}
+						</p>
+						<Form>
+							<FormGroup controlId="form-token">
+								<Row>
+									<Col sm={3}>
+										<FormLabel>{t('token')}</FormLabel>
+									</Col>
+									<Col sm={9}>
+										<FormControl
+											type="text"
+											placeholder={t('enter-token')}
+											onChange={( event ) => {
+												setToken( event.target.value );
+											}}
+										/>
+									</Col>
+								</Row>
+							</FormGroup>
+							<Button
+								className="centered"
+								variant="primary"
+								onClick={handleSubmit}
+								type="submit"
+								disabled={token.length !== 6}
+							>{t('common:login')}</Button>
+						</Form>
+					</Card.Body>
+				</Card>
+				{settings.brandingLogo ? <img
+					className="login-branding-logo"
+					src={settings.brandingLogo}
+					alt="Branded Logo"
+				/> : null}
+			</div>
+		</Fragment>
+	);
+};
 
 
 // PROPERTIES //
@@ -134,4 +134,4 @@ LoginTFA.defaultProps = {
 
 // EXPORTS //
 
-export default withTranslation( [ 'login', 'common' ] )( LoginTFA );
+export default LoginTFA;
