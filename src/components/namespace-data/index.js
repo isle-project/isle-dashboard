005fffea6f50e801a05b3e9001a60b2ac552bd5b
Planeshifter <pburckhardt@outlook.com>
2021-11-15 12:34:05 -0500

    Rewrite using hooks

--- src/components/namespace-data/index.js
+++ src/components/namespace-data/index.js
@@ -17,10 +17,11 @@
 
 // MODULES //
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import logger from 'debug';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { useParams, useNavigate } from 'react-router-dom';
 import Alert from 'react-bootstrap/Alert';
 import Nav from 'react-bootstrap/Nav';
 import FilesPage from './files_page.js';
@@ -40,191 +41,175 @@ const debug = logger( 'isle-dashboard:namespace-data' );
 
 // MAIN //
 
-class NamespaceData extends Component {
-	constructor( props ) {
-		super( props );
-
-		const subpage = props.match.params.subpage;
-		let activePage;
-		switch ( subpage ) {
-			default:
-			case 'announcements':
-				activePage = 1;
-				break;
-			case 'progress':
-				activePage = 2;
-				break;
-			case 'grades':
-				activePage = 3;
-				break;
-			case 'cohorts':
-				activePage = 4;
-				break;
-			case 'owner-files':
-				activePage = 5;
-				break;
-			case 'student-files':
-				activePage = 6;
-				break;
-			case 'tickets':
-				activePage = 7;
-				break;
-			case 'actions':
-				activePage = 8;
-				break;
-		}
-		this.state = {
-			activePage
-		};
+const NamespaceData = ({
+		addAnnouncement, deleteAnnouncement, editAnnouncement, badges, namespace, user,
+		getBadges, uploadFile, deleteFile, getFiles, sendTicketMessage, addNotification,
+		adjustProgress, getCourseTickets, getNamespaceActions, openTicket, closeTicket,
+		updatePriority, getOwnerFiles
+}) => {
+	const subpage = useParams().subpage;
+	const [ activePage, setActivePage ] = useState( subpage );
+	const { t } = useTranslation( [ 'namespace_data', 'common' ] );
+	const navigate = useNavigate();
+	useEffect( () => {
+		debug( 'Getting badges upon mounting...' );
+		getBadges();
+	}, [] );
+	if ( !namespace._id ) {
+		return ( <div className="namespace-data-div">
+			<Alert variant="danger">{t('common:no-course-selected')}</Alert>
+		</div> );
 	}
-
-	componentDidMount() {
-		this.props.getBadges();
-	}
-
-	handleUpload = ( event ) => {
+	const handleUpload = ( event ) => {
 		debug( 'Uploading file...' );
 		const file = event.target.files[ 0 ];
 		const formData = new FormData();
 		formData.append( 'file', file, file.name );
-		formData.append( 'namespaceName', this.props.namespace.title );
+		formData.append( 'namespaceName', namespace.title );
 		formData.append( 'owner', 'true' );
-		this.props.uploadFile({
-			formData: formData,
-			user: this.props.user
-		});
+		uploadFile({ formData, user });
 	};
-
-	handleAnnouncementDeletion = (createdAt, index) => {
-		this.props.deleteAnnouncement( {
-			namespaceName: this.props.namespace.title,
+	const handleAnnouncementDeletion = ( createdAt, index ) => {
+		deleteAnnouncement( {
+			namespaceName: namespace.title,
 			index,
 			createdAt
 		});
 	};
-
-	handleAnnouncementEdit = ( announcement ) => {
-		this.props.editAnnouncement({
-			namespaceName: this.props.namespace.title,
+	const handleAnnouncementEdit = ( announcement ) => {
+		editAnnouncement({
+			namespaceName: namespace.title,
 			announcement
 		});
 	};
-
-	handleAnnouncementCreation = ( announcement ) => {
-		this.props.addAnnouncement({
-			namespaceName: this.props.namespace.title,
+	const handleAnnouncementCreation = ( announcement ) => {
+		addAnnouncement({
+			namespaceName: namespace.title,
 			announcement
 		});
 	};
-
-	handleFileDeletion = ( _id, ownerFiles ) => {
-		this.props.deleteFile( _id, this.props.namespace.title, ownerFiles );
+	const handleFileDeletion = ( _id, ownerFiles ) => {
+		deleteFile( _id, namespace.title, ownerFiles );
 	};
-
-	submitTicketMessage = ({ message, ticketID }) => {
-		this.props.sendTicketMessage({ message, ticketID, user: this.props.user });
+	const submitTicketMessage = ({ message, ticketID }) => {
+		sendTicketMessage({ message, ticketID, user: user });
 	};
-
-	handleSelect = ( selectedKey ) => {
+	let page;
+	switch ( activePage ) {
+		case 1:
+			page = <AnnouncementsPage
+				namespace={namespace} editAnnouncement={handleAnnouncementEdit}
+				deleteAnnouncement={handleAnnouncementDeletion}
+				addAnnouncement={handleAnnouncementCreation} user={user}
+			/>;
+			break;
+		case 2:
+			page = <ProgressPage adjustProgress={adjustProgress} addNotification={addNotification}
+				namespace={namespace} cohorts={namespace.cohorts}
+				lessons={namespace.lessons} user={user}
+			/>;
+			break;
+		case 3:
+			page = <GradesPage addNotification={addNotification} namespace={namespace}
+				cohorts={namespace.cohorts} lessons={namespace.lessons} user={user}
+			/>;
+			break;
+		case 4:
+			page = <CohortsPage badges={badges} cohorts={namespace.cohorts}
+				lessons={namespace.lessons}
+			/>;
+			break;
+		case 5:
+			page = <FilesPage ownerFiles files={namespace.ownerFiles} namespace={namespace}
+				handleUpload={handleUpload} handleFileDeletion={handleFileDeletion}
+				addNotification={addNotification} getFiles={getOwnerFiles}
+			/>;
+			break;
+		case 6:
+			page = <FilesPage files={namespace.files} namespace={namespace} handleUpload={handleUpload}
+				handleFileDeletion={handleFileDeletion} addNotification={addNotification} getFiles={getFiles}
+			/>;
+			break;
+		case 7:
+			page = <TicketsPage tickets={namespace.tickets} namespace={namespace}
+				getCourseTickets={getCourseTickets} submitTicketMessage={submitTicketMessage}
+				openTicket={openTicket} closeTicket={closeTicket}
+				updatePriority={updatePriority}
+			/>;
+			break;
+		case 8:
+			page = <ActionsPage namespace={namespace} getNamespaceActions={getNamespaceActions}
+				user={user} cohorts={namespace.cohorts}
+			/>;
+			break;
+	}
+	const handleSelect = ( selectedKey ) => {
 		selectedKey = Number( selectedKey );
-		const title = this.props.namespace.title;
+		const title = namespace.title;
 		switch ( selectedKey ) {
 			case 1:
-				this.props.history.replace( `/namespace-data/${title}/announcements` );
+				navigate( `/namespace-data/${title}/announcements` );
 				break;
 			case 2:
-				this.props.history.replace( `/namespace-data/${title}/progress` );
+				navigate( `/namespace-data/${title}/progress` );
 				break;
 			case 3:
-				this.props.history.replace( `/namespace-data/${title}/grades` );
+				navigate( `/namespace-data/${title}/grades` );
 				break;
 			case 4:
-				this.props.history.replace( `/namespace-data/${title}/cohorts` );
+				navigate( `/namespace-data/${title}/cohorts` );
 				break;
 			case 5:
-				this.props.history.replace( `/namespace-data/${title}/owner-files` );
+				navigate( `/namespace-data/${title}/owner-files` );
 				break;
 			case 6:
-				this.props.history.replace( `/namespace-data/${title}/student-files` );
+				navigate( `/namespace-data/${title}/student-files` );
 				break;
 			case 7:
-				this.props.history.replace( `/namespace-data/${title}/tickets` );
+				navigate( `/namespace-data/${title}/tickets` );
 				break;
 			case 8:
-				this.props.history.replace( `/namespace-data/${title}/actions` );
+				navigate( `/namespace-data/${title}/actions` );
 				break;
 		}
-		this.setState({
-			activePage: selectedKey
-		});
+		setActivePage( selectedKey );
 	};
-
-	renderPage() {
-		switch ( this.state.activePage ) {
-			case 1:
-				return <AnnouncementsPage namespace={this.props.namespace} editAnnouncement={this.handleAnnouncementEdit} deleteAnnouncement={this.handleAnnouncementDeletion} addAnnouncement={this.handleAnnouncementCreation} user={this.props.user} />;
-			case 2:
-				return <ProgressPage adjustProgress={this.props.adjustProgress} addNotification={this.props.addNotification} namespace={this.props.namespace} cohorts={this.props.namespace.cohorts} lessons={this.props.namespace.lessons} user={this.props.user} />;
-			case 3:
-				return <GradesPage addNotification={this.props.addNotification} namespace={this.props.namespace} cohorts={this.props.namespace.cohorts} lessons={this.props.namespace.lessons} user={this.props.user} />;
-			case 4:
-				return <CohortsPage badges={this.props.badges} cohorts={this.props.namespace.cohorts} lessons={this.props.namespace.lessons} />;
-			case 5:
-				return <FilesPage ownerFiles files={this.props.namespace.ownerFiles} namespace={this.props.namespace} handleUpload={this.handleUpload} handleFileDeletion={this.handleFileDeletion} addNotification={this.props.addNotification} getFiles={this.props.getOwnerFiles} />;
-			case 6:
-				return <FilesPage files={this.props.namespace.files} namespace={this.props.namespace} handleUpload={this.handleUpload} handleFileDeletion={this.handleFileDeletion} addNotification={this.props.addNotification} getFiles={this.props.getFiles} />;
-			case 7:
-				return <TicketsPage tickets={this.props.namespace.tickets} namespace={this.props.namespace} getCourseTickets={this.props.getCourseTickets} submitTicketMessage={this.submitTicketMessage} openTicket={this.props.openTicket} closeTicket={this.props.closeTicket} updatePriority={this.props.updatePriority} history={this.props.history} />;
-			case 8:
-				return <ActionsPage namespace={this.props.namespace} getNamespaceActions={this.props.getNamespaceActions} user={this.props.user} cohorts={this.props.namespace.cohorts} />;
-		}
-	}
-
-	render() {
-		const { t } = this.props;
-		if ( !this.props.namespace._id ) {
-			return ( <div className="namespace-data-div">
-				<Alert variant="danger">{t('common:no-course-selected')}</Alert>
-			</div> );
-		}
-		const page = this.renderPage();
-		return (
-			<div className="namespace-data-div">
-				<div className="namespace-data-navbar">
-					<Nav variant="pills" className="namespace-data-navbar-pills" activeKey={this.state.activePage} onSelect={this.handleSelect}>
-						<Nav.Item>
-							<Nav.Link eventKey="1" title="Announcements" >{t('common:announcements')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="2" title="Progress" >{t('common:progress')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="3" title="Grades" >{t('grades')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="4" title="Cohorts" >{t('common:cohorts')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="5" title="Owner Files" >{t('owner-files')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="6" title="Student Files" >{t('student-files')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="7" title="Tickets" disabled={!this.props.namespace.enableTicketing} >{t('common:tickets')}</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<Nav.Link eventKey="8" title="Actions" disabled >{t('common:actions')}</Nav.Link>
-						</Nav.Item>
-					</Nav>
-				</div>
-				<div className="namespace-data-page-container" style={{ overflowY: 'auto' }}>
-					{page}
-				</div>
+	return (
+		<div className="namespace-data-div">
+			<div className="namespace-data-navbar">
+				<Nav variant="pills" className="namespace-data-navbar-pills" activeKey={activePage} onSelect={handleSelect}>
+					<Nav.Item>
+						<Nav.Link eventKey="announcements" title="Announcements" >{t('common:announcements')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="progress" title="Progress" >{t('common:progress')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="grades" title="Grades" >{t('grades')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="cohorts" title="Cohorts" >{t('common:cohorts')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="owner-files" title="Owner Files" >{t('owner-files')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="student-files" title="Student Files" >{t('student-files')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="tickets" title="Tickets" disabled={!namespace.enableTicketing} >{t('common:tickets')}</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link eventKey="actions" title="Actions" disabled >{t('common:actions')}</Nav.Link>
+					</Nav.Item>
+				</Nav>
 			</div>
-		);
-	}
-}
+			<div className="namespace-data-page-container" style={{ overflowY: 'auto' }}>
+				{page}
+			</div>
+		</div>
+	);
+};
 
 
 // PROPERTIES //
@@ -243,7 +228,6 @@ NamespaceData.propTypes = {
 	getFiles: PropTypes.func.isRequired,
 	getNamespaceActions: PropTypes.func.isRequired,
 	getOwnerFiles: PropTypes.func.isRequired,
-	match: PropTypes.object.isRequired,
 	namespace: PropTypes.object.isRequired,
 	openTicket: PropTypes.func.isRequired,
 	sendTicketMessage: PropTypes.func.isRequired,
@@ -259,4 +243,4 @@ NamespaceData.defaultProps = {
 
 // EXPORTS //
 
-export default withTranslation( [ 'namespace_data', 'common' ] )( NamespaceData );
+export default NamespaceData;
