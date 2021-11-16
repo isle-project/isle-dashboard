0545778696122ab6cc4bc5b1cab650032226f018
Planeshifter <pburckhardt@outlook.com>
2021-11-16 18:02:11 -0500

    Remove unused variable

--- test/components/login.spec.js
+++ test/components/login.spec.js
@@ -19,7 +19,6 @@
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import noop from '@stdlib/utils/noop';
@@ -29,8 +28,6 @@ import Login from './../../src/components/login';
 // TESTS //
 
 describe( '<Login />', function test() {
-	const history = createMemoryHistory();
-
 	it( 'should render without throwing an error', () => {
 		render(
 			<BrowserRouter>
