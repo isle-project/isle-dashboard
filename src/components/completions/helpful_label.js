12dc520322d64d765f6607b948a1235514fd9971
Philipp Burckhardt <pburckhardt@outlook.com>
2022-08-12 16:39:12 -0400

    fix: Add keys

--- src/components/completions/helpful_label.js
+++ src/components/completions/helpful_label.js
@@ -9,11 +9,11 @@ import Form from 'react-bootstrap/Form';
 // MAIN //
 
 function HelpfulLabel({ name, description, colWidth, className, placement = 'left', disabled = false, as = Form.Label }) {
-	const children = [ name ];
+	const children = [ <span key="name" >{name}</span> ];
 	if ( disabled ) {
 		return React.createElement( as, { sm: colWidth, className: className, column: true }, children );
 	}
-	children.push( <i className="info-icon ms-2 fas fa-info"></i> );
+	children.push( <i key="info-icon" className="info-icon ms-2 fas fa-info"></i> );
 	return (
 		<OverlayTrigger placement={placement} overlay={<Tooltip id={`param-${name}-tooltip`}>{description}</Tooltip>} >
 			{React.createElement( as, { sm: colWidth, className: className, column: true }, children )}
